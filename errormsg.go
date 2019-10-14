5f01e6ec5145df9a8aedd7fc6af884e3dd3b07bc
Alexander F. Rødseth <rodseth@gmail.com>
2019-10-14 17:21:05 +0200

    Add a new error message function

--- errormsg.go
+++ errormsg.go
@@ -6,12 +6,22 @@ import (
 	"os"
 )
 
-func errorMessageQuit(tty *vt100.TTY, err error) {
+func quitError(tty *vt100.TTY, err error) {
 	tty.Close()
 	vt100.Reset()
 	vt100.Clear()
 	vt100.Close()
 	fmt.Fprintln(os.Stderr, "error: "+err.Error())
+	vt100.SetXY(uint(0), uint(1))
+	os.Exit(1)
+}
+
+func quitMessage(tty *vt100.TTY, msg string) {
+	tty.Close()
+	vt100.Reset()
+	vt100.Clear()
+	vt100.Close()
+	fmt.Fprintln(os.Stderr, msg)
 	vt100.SetXY(uint(0), uint(1))
 	os.Exit(1)
 }
