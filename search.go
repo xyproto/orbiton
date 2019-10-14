c204fba7a22ea82f275a7e2e6bb5beb3a707c9ea
Alexander F. Rødseth <rodseth@gmail.com>
2019-10-14 12:58:03 +0200

    Add a simple function

--- search.go
+++ search.go
@@ -4,6 +4,7 @@ import (
 	"github.com/xyproto/vt100"
 )
 
+// SetSearchTerm will set the current search term to highlight
 func (e *Editor) SetSearchTerm(s string, c *vt100.Canvas) {
 	// set the search term
 	e.searchTerm = s
@@ -11,4 +12,9 @@ func (e *Editor) SetSearchTerm(s string, c *vt100.Canvas) {
 	h := int(c.Height())
 	e.WriteLines(c, e.pos.Offset(), h+e.pos.Offset(), 0, 0)
 	c.Draw()
+}
+
+// SearchTerm will return the current search term
+func (e *Editor) SearchTerm() string {
+	return e.searchTerm
 }
