3d200b06f555eddb7fe3cc79776f91c08f2c5f53
Alexander F. Rødseth <rodseth@gmail.com>
2019-11-09 13:40:04 +0100

    Add a mutex to the resize handler

--- resize.go
+++ resize.go
@@ -3,13 +3,17 @@ package main
 import (
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 
 	"github.com/xyproto/vt100"
 )
 
+var resizeMutex sync.RWMutex
+
 // SetUpResizeHandler sets up a signal handler for when the terminal is resized
 func SetUpResizeHandler(c *vt100.Canvas, e *Editor, tty *vt100.TTY) {
+	resizeMutex.Lock()
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, syscall.SIGWINCH)
 	go func() {
@@ -19,4 +23,5 @@ func SetUpResizeHandler(c *vt100.Canvas, e *Editor, tty *vt100.TTY) {
 			e.redrawCursor = true
 		}
 	}()
+	resizeMutex.Unlock()
 }
