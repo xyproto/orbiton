5ff1457403123d9fd95a589dc6d569f9c6bf87e6
Alexander F. Rødseth <rodseth@gmail.com>
2020-01-25 21:03:26 +0100

    Remove comments

--- render.go
+++ render.go
@@ -7,55 +7,7 @@ import (
 	"time"
 
 	"github.com/jung-kurt/gofpdf"
-	//"github.com/xyproto/burnfont"
 )
-
-//// SavePNG will render the current text to a .png image, using a fixed font
-//func (e *Editor) SavePNG(filename string) error {
-//
-//	// Find the longest line
-//	maxlen := 0
-//	for i := 0; i < e.Len(); i++ {
-//		line := e.Line(i)
-//		if len(line) > maxlen {
-//			maxlen = len(line)
-//		}
-//	}
-//
-//	lineHeight := 14
-//	marginRight := 4 * lineHeight
-//	width := 8*(maxlen+1) + marginRight
-//	height := (e.Len() * lineHeight) + 2*lineHeight
-//
-//	dimension := image.Rectangle{image.Point{}, image.Point{width, height}}
-//
-//	textImage := image.NewRGBA(dimension)
-//	finalImage := image.NewRGBA(dimension)
-//
-//	darkgray := color.NRGBA{0x10, 0x10, 0x10, 0xff}
-//	white := color.NRGBA{0xff, 0xff, 0xff, 0xff}
-//
-//	draw.Draw(finalImage, finalImage.Bounds(), &image.Uniform{white}, image.Point{}, draw.Src)
-//
-//	// For each line of this text document, draw the string to an image
-//	var contents string
-//	for i := 0; i < e.Len(); i++ {
-//		// Expand tabs for each line
-//		contents = strings.Replace(e.Line(i), "\t", strings.Repeat(" ", e.spacesPerTab), -1)
-//		// Draw the string to the textImage
-//		burnfont.DrawString(textImage, lineHeight, (i+1)*lineHeight, contents, darkgray)
-//	}
-//
-//	// Now overlay the text image on top of the final image with the background color
-//	draw.Draw(finalImage, finalImage.Bounds(), textImage, image.Point{}, draw.Over)
-//
-//	// Write the PNG file
-//	f, err := os.Create(filename)
-//	if err != nil {
-//		return err
-//	}
-//	return png.Encode(f, finalImage)
-//}
 
 // SavePDF can save the text as a PDF. It's pretty experimental.
 func (e *Editor) SavePDF(title, filename string) error {
