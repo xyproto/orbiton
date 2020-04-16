fe2e61ce09fb8e46e15e117a89b560398b9ff2e8
Alexander F. Rødseth <rodseth@gmail.com>
2020-04-16 13:57:45 +0200

    Update dependencies

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.10
 require (
 	github.com/atotto/clipboard v0.1.2
 	github.com/jung-kurt/gofpdf v1.16.2
-	github.com/xyproto/syntax v1.5.0
+	github.com/xyproto/syntax v1.6.0
 	github.com/xyproto/textoutput v1.11.0
 	github.com/xyproto/vt100 v1.8.3
 	golang.org/x/sys v0.0.0-20200413165638-669c56c373c4 // indirect
--- vendor/github.com/xyproto/syntax/highlight.go
+++ vendor/github.com/xyproto/syntax/highlight.go
@@ -6,7 +6,6 @@ package syntax
 import (
 	"bytes"
 	"io"
-	"strings"
 	"text/scanner"
 	"unicode"
 	"unicode/utf8"
@@ -249,8 +248,8 @@ func NewScannerReader(src io.Reader) *scanner.Scanner {
 }
 
 func tokenKind(tok rune, tokText string, inSingleLineComment *bool) Kind {
-	// Check if we are in a bash-style single line comment, or assembly-style single line comments
-	if tok == '#' || (tok == ';' && strings.HasPrefix(tokText, ";")) {
+	// Check if we are in a bash-style single line comment
+	if tok == '#' {
 		*inSingleLineComment = true
 	} else if tok == '\n' {
 		*inSingleLineComment = false
