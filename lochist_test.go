78a9136f93a91d73aec8dbe4bec80e31614fc708
Alexander F. Rødseth <rodseth@gmail.com>
2020-05-12 16:11:53 +0200

    Add a test

--- lochist_test.go
+++ lochist_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -10,4 +13,21 @@ func TestVimInfo(t *testing.T) {
 
 func TestEmacsPlaces(t *testing.T) {
 	LoadEmacsLocationHistory(expandUser(emacsLocationHistoryFilename))
+}
+
+func TestNeoVimMsgPack(t *testing.T) {
+	curdir, err := os.Getwd()
+	if err != nil {
+		t.Fail()
+	}
+	searchFilename, err := filepath.Abs(filepath.Join(curdir, "main.go"))
+	if err != nil {
+		t.Fail()
+	}
+	line, err := FindInNvimLocationHistory(expandUser(nvimLocationHistoryFilename), searchFilename)
+	if err != nil {
+		// main.go might not be in the neovim location history, this is fine
+		fmt.Println(err)
+	}
+	fmt.Println("line", line)
 }
