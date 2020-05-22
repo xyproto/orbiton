bf2e32eb9fde4356166a039298b4de0179d0ac73
Alexander F. Rødseth <rodseth@gmail.com>
2020-05-22 16:15:43 +0200

    Minor changes to the terminal resize handler

--- resize.go
+++ resize.go
@@ -12,9 +12,13 @@ import (
 var resizeMutex sync.RWMutex
 
 // SetUpResizeHandler sets up a signal handler for when the terminal is resized
-func SetUpResizeHandler(c *vt100.Canvas, e *Editor, status *StatusBar, tty *vt100.TTY) {
+func (e *Editor) SetUpResizeHandler(c *vt100.Canvas, status *StatusBar, tty *vt100.TTY) {
 	resizeMutex.Lock()
 	sigChan := make(chan os.Signal, 1)
+
+	// Clear any previous resize handlers
+	signal.Reset(syscall.SIGWINCH)
+
 	signal.Notify(sigChan, syscall.SIGWINCH)
 	go func() {
 		for range sigChan {
