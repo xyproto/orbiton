34775734c4f0cfb7c6793058514af5965243dc65
Alexander F. Rødseth <rodseth@gmail.com>
2020-05-23 18:50:50 +0200

    Add a test

--- build_test.go
+++ build_test.go
@@ -8,23 +8,20 @@ import (
 
 func ExampleEditor_BuildOrExport_goError() {
 	e := NewSimpleEditor(80)
-	os.Chdir("tests")
-
+	os.Chdir("test")
 	// The rename is so that "err.go" is not picked up by the CI tests
 	os.Rename("err_go", "err.go")
 	s, performedAction, compiledOK := e.BuildOrExport(nil, nil, "err.go")
 	os.Rename("err.go", "err_go")
-
 	os.Chdir("..")
 	fmt.Printf("%s [performed action: %v] [compiled OK: %v]\n", s, performedAction, compiledOK)
-
 	// Output:
 	// undefined: asdfasdf [performed action: true] [compiled OK: false]
 }
 
 func TestBuildOrExport(t *testing.T) {
 	e := NewSimpleEditor(80)
-	os.Chdir("tests")
+	os.Chdir("test")
 	_, performedAction, compiledOK := e.BuildOrExport(nil, nil, "err.rs")
 	os.Chdir("..")
 
@@ -49,4 +46,17 @@ func TestBuildOrExport(t *testing.T) {
 			t.Fail()
 		}
 	}
+}
+
+func ExampleEditor_BuildOrExport_goTest() {
+	e := NewSimpleEditor(80)
+	os.Chdir("test")
+	// The rename is so that "err.go" is not picked up by the CI tests
+	os.Rename("err_test_go", "err_test.go")
+	s, performedAction, compiledOK := e.BuildOrExport(nil, nil, "err_test.go")
+	os.Rename("err_test.go", "err_test_go")
+	os.Chdir("..")
+	fmt.Printf("%s [performed action: %v] [compiled OK: %v]\n", s, performedAction, compiledOK)
+	// Output:
+	// Test failed: TestTest (0.00s) [performed action: true] [compiled OK: false]
 }
