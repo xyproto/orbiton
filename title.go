fa61fd1877c5174ee02929b2580f83ce69129d5b
Alexander F. Rødseth <rodseth@gmail.com>
2021-03-16 11:42:04 +0100

    Minor changes

--- title.go
+++ title.go
@@ -5,22 +5,23 @@ import (
 	"path/filepath"
 )
 
+// generateTitle tries to find a suitable terminal emulator title text for a given filename,
+// that is not too long (ideally <30 characters)
 func generateTitle(filename string) string {
 	absPath, err := filepath.Abs(filename)
 	if err != nil {
 		return filepath.Base(filename)
 	}
 	// First try to find the relative path to the home directory
-	if relPath, err := filepath.Rel(os.Getenv("HOME"), absPath); err != nil {
+	relPath, err := filepath.Rel(os.Getenv("HOME"), absPath)
+	if err != nil {
 		// If the relative directory to $HOME could not be found, then just use the base filename
 		return filepath.Base(filename)
-	} else {
-		title := filepath.Join("~", relPath)
-		// If the relative directory path is too long, just use the base filename
-		if len(title) >= 30 {
-			title = filepath.Base(filename)
-		}
-		return title
 	}
-	return "o"
+	title := filepath.Join("~", relPath)
+	// If the relative directory path is too long, just use the base filename
+	if len(title) >= 30 {
+		title = filepath.Base(filename)
+	}
+	return title
 }
