d15cc22e1ecdfae0cf139f2b513129626d1d191a
Alexander F. Rødseth <rodseth@gmail.com>
2021-05-12 17:39:20 +0200

    Use different colors for highlighting the pointer arrows

--- arrow.go
+++ arrow.go
@@ -6,13 +6,10 @@ import (
 	"github.com/xyproto/syntax"
 )
 
-var (
-	arrowColor = syntax.DefaultTextConfig.Dollar
-	fieldColor = syntax.DefaultTextConfig.Protected
-)
-
 // Syntax highlight pointer arrows in C and C++
 func arrowReplace(s string) string {
+	arrowColor := syntax.DefaultTextConfig.Dollar
+	fieldColor := syntax.DefaultTextConfig.Protected
 	s = strings.Replace(s, ">-<", "><off><"+arrowColor+">-<", -1)
 	s = strings.Replace(s, ">"+escapedGreaterThan, "><off><"+arrowColor+">"+escapedGreaterThan+"<off><"+fieldColor+">", -1)
 	return s
