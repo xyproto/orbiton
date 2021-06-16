e01ab4250117c14e76ec417ada0cdcccddae4a66
Alexander F. Rødseth <rodseth@gmail.com>
2021-06-16 11:27:41 +0200

    Refactor

--- highlight.go
+++ highlight.go
@@ -431,3 +431,12 @@ func (e *Editor) WriteLines(c *vt100.Canvas, fromline, toline LineIndex, cx, cy 
 
 	return nil
 }
+
+// Syntax highlight pointer arrows in C and C++
+func arrowReplace(s string) string {
+	arrowColor := syntax.DefaultTextConfig.Class
+	fieldColor := syntax.DefaultTextConfig.Protected
+	s = strings.Replace(s, ">-<", "><off><"+arrowColor+">-<", -1)
+	s = strings.Replace(s, ">"+Escape(">"), "><off><"+arrowColor+">"+Escape(">")+"<off><"+fieldColor+">", -1)
+	return s
+}
