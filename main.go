bdf7ceb00a4f68756933da27ae6ca1518dc3b786
Alexander F. Rødseth <rodseth@gmail.com>
2021-08-09 23:20:00 +0200

    New patch release

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ import (
 )
 
 const (
-	versionString = "o 2.41.1"
+	versionString = "o 2.41.2"
 )
 
 func main() {
--- version.sh
+++ version.sh
@@ -4,7 +4,7 @@
 #
 
 # The current version goes here, as the default value
-VERSION=${1:-'2.41.1'}
+VERSION=${1:-'2.41.2'}
 
 if [ -z "$1" ]; then
   echo "The current version is $VERSION, pass the new version as the first argument if you wish to change it"
