dee08b7789da7202db7f579acff33d33f0c06b5a
Alexander F. Rødseth <rodseth@gmail.com>
2021-10-08 11:49:50 +0200

    Update tests

--- build_test.go
+++ build_test.go
@@ -15,10 +15,10 @@ func ExampleEditor_BuildOrExport_goError() {
 	os.Chdir("test")
 	// The rename is so that "err.go" is not picked up by the CI tests
 	os.Rename("err_go", "err.go")
-	s, performedAction, compiledOK := e.BuildOrExport(nil, nil, nil, "err.go")
+	s, performedAction, compiledOK, outputExecutable := e.BuildOrExport(nil, nil, nil, "err.go")
 	os.Rename("err.go", "err_go")
 	os.Chdir("..")
-	fmt.Printf("%s [performed action: %v] [compiled OK: %v]\n", s, performedAction, compiledOK)
+	fmt.Printf("%s [performed action: %v] [compiled OK: %v] %s\n", s, performedAction, compiledOK, outputExecutable)
 	// Output:
 	// undefined: asdfasdf [performed action: true] [compiled OK: false]
 }
@@ -28,7 +28,7 @@ func TestBuildOrExport(t *testing.T) {
 	e.mode = mode.Detect("err.rs")
 
 	os.Chdir("test")
-	_, performedAction, compiledOK := e.BuildOrExport(nil, nil, nil, "err.rs")
+	_, performedAction, compiledOK, _ := e.BuildOrExport(nil, nil, nil, "err.rs")
 	os.Chdir("..")
 
 	// fmt.Printf("%s [performed action: %v] [compiled OK: %v]\n", s, performedAction, compiledOK)
@@ -61,10 +61,10 @@ func ExampleEditor_BuildOrExport_goTest() {
 	os.Chdir("test")
 	// The rename is so that "err.go" is not picked up by the CI tests
 	os.Rename("err_test_go", "err_test.go")
-	s, performedAction, compiledOK := e.BuildOrExport(nil, nil, nil, "err_test.go")
+	s, performedAction, compiledOK, outputExecutable := e.BuildOrExport(nil, nil, nil, "err_test.go")
 	os.Rename("err_test.go", "err_test_go")
 	os.Chdir("..")
-	fmt.Printf("%s [performed action: %v] [compiled OK: %v]\n", s, performedAction, compiledOK)
+	fmt.Printf("%s [performed action: %v] [compiled OK: %v] %s\n", s, performedAction, compiledOK, outputExecutable)
 	// Output:
 	// Test failed: TestTest (0.00s) [performed action: true] [compiled OK: false]
 }
