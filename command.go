9177971241158293426609f675f1f1b74e950420
Alexander F. Rødseth <rodseth@gmail.com>
2021-10-08 14:13:56 +0200

    Move the debug functionality

--- command.go
+++ command.go
@@ -11,7 +11,6 @@ import (
 	"strings"
 	"time"
 
-	"github.com/cyrus-and/gdb"
 	"github.com/xyproto/env"
 	"github.com/xyproto/guessica"
 	"github.com/xyproto/vt100"
@@ -255,20 +254,10 @@ func (e *Editor) CommandMenu(c *vt100.Canvas, tty *vt100.TTY, status *StatusBar,
 		})
 	} else {
 		actions.Add("Debug mode", func() {
-
-			gdb, err := gdb.New(nil)
-			if err != nil {
-				status.ClearAll(c)
-				status.SetMessage(err.Error())
-				status.Show(c, e)
-				return
-			}
-
+			e.debugMode = true
 			status.Clear(c)
 			status.SetMessage("Debug mode enabled")
 			status.Show(c, e)
-			e.debugMode = true
-			e.gdb = gdb
 		})
 	}
 
