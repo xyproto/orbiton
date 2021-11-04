22028f802ebb340d83c8006cc03ca80bab9b61ed
Alexander F. Rødseth <rodseth@gmail.com>
2021-11-04 10:48:10 +0100

    Fix a typo in a comment

--- ko/main.cpp
+++ ko/main.cpp
@@ -534,7 +534,7 @@ int main(int argc, char* argv[])
         return EXIT_FAILURE;
     }
 
-    // Unset the DISPLAY environment variable, which affects the behavior of "o"
+    // Set the DISPLAY environment variable, which affects the behavior of "o"
     setenv("KO", "1", true);
 
     // Spawn a terminal
