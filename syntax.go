8559b61000f74d5015820c4faba27fa55151bfe5
Alexander F. Rødseth <rodseth@gmail.com>
2021-11-22 11:25:37 +0100

    Improve the shell syntax highlighting

--- syntax.go
+++ syntax.go
@@ -136,7 +136,7 @@ func adjustSyntaxHighlightingKeywords(m mode.Mode) {
 		clearKeywords()
 		addKeywords = []string{"INT", "SYSCALL", "int", "syscall"}
 	case mode.Shell:
-		addKeywords = []string{"--force", "-f", "clean", "cmake", "configure", "dd", "do", "doas", "endif", "fdisk", "for", "gdisk", "in", "make", "mv", "ninja", "rm", "rmdir", "setopt", "su", "sudo", "while"}
+		addKeywords = []string{"--force", "-f", "checkout", "clean", "cmake", "configure", "dd", "do", "doas", "endif", "fdisk", "for", "gdisk", "in", "make", "mv", "ninja", "rm", "rmdir", "setopt", "su", "sudo", "while"}
 		delKeywords = []string{"#else", "#endif", "default", "double", "exec", "float", "fn", "generic", "get", "install", "long", "new", "no", "pass", "property", "ret", "super", "template", "var", "with"}
 		fallthrough // to the default case
 	default:
