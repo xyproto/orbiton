b5a164c654fa63e272d6886ecf7b340fb93cc5a3
Alexander F. Rødseth <rodseth@gmail.com>
2022-04-05 13:04:59 +0200

    Improve the Python syntax highlighting

--- v2/syntax.go
+++ v2/syntax.go
@@ -110,7 +110,7 @@ func adjustSyntaxHighlightingKeywords(m mode.Mode) {
 		addKeywords = []string{"fn"}
 		delKeywords = []string{"from", "new", "print"}
 	case mode.Python:
-		delKeywords = []string{"fn"}
+		delKeywords = []string{"append", "fn", "get", "package", "print"}
 	case mode.Odin:
 		clearKeywords()
 		addKeywords = odinWords
