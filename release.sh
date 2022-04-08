5d35971f6e8588938f39d527a57b2c32fca86579
Alexander F. Rødseth <rodseth@gmail.com>
2022-04-08 18:06:11 +0200

    Update the release script

--- release.sh
+++ release.sh
@@ -1,39 +1,68 @@
 #!/bin/sh
 #
-# Create release tarballs/zip for 64-bit linux, BSD and Plan9 + 64-bit ARM + raspberry pi 2/3
+# Create release tarballs/zip-files
 #
 cd v2
 name=o
 version=$(grep -i version main.go | head -1 | cut -d' ' -f4 | cut -d'"' -f1)
 echo "Version $version"
+
+export GOBUILD=( go build -mod=vendor -trimpath -ldflags "-w -s" -a -o )
+export CGO_ENABLED=0
+
 echo 'Compiling...'
-export GOARCH=amd64
-echo '* Linux'
-GOOS=linux go build -mod=vendor -o $name.linux_amd64
-echo '* macOS'
-GOOS=darwin go build -mod=vendor -o $name.macos_amd64
-echo '* FreeBSD'
-GOOS=freebsd go build -mod=vendor -o $name.freebsd_amd64
-echo '* NetBSD'
-GOOS=netbsd go build -mod=vendor -o $name.netbsd_amd64
-# OpenBSD support: https://github.com/pkg/term/issues/27
-#echo '* OpenBSD'
-#GOOS=openbsd go build -mod=vendor -o $name.openbsd
+echo '* Linux x86_64'
+export GOOS=linux
+GOARCH=amd64 "${GOBUILD[@]}" $name.linux_x86_64_static
+echo '* Linux aarch64'
+export GOARCH=arm64
+"${GOBUILD[@]}" $name.linux_aarch64_static
+export GOARCH=arm
+echo '* Linux armv6'
+GOARM=6 "${GOBUILD[@]}" $name.linux_armv6_static
 echo '* Linux armv7 (RPI 2/3/4)'
-GOOS=linux GOARCH=arm GOARM=7 go build -mod=vendor -o $name.linux_rpi234
-echo '* Linux amd64 static w/ upx'
-GOOS=linux CGO_ENABLED=0 GOARCH=amd64 go build -mod=vendor -trimpath -ldflags "-s" -a -o $name.linux_amd64_static && upx $name.linux_amd64_static
-echo '* Linux arm64 static'
-GOOS=linux CGO_ENABLED=0 GOARCH=arm64 go build -mod=vendor -trimpath -ldflags "-s" -a -o $name.linux_arm64_static
-echo '* Linux armv6 static'
-GOOS=linux CGO_ENABLED=0 GOARCH=arm GOARM=6 go build -mod=vendor -trimpath -ldflags "-s" -a -o $name.linux_armv6_static
-echo '* Linux armv7 static'
-GOOS=linux CGO_ENABLED=0 GOARCH=arm GOARM=7 go build -mod=vendor -trimpath -ldflags "-s" -a -o $name.linux_armv7_static
-echo '* Darwin arm64 static'
-GOOS=darwin CGO_ENABLED=0 GOARCH=arm64 go build -mod=vendor -trimpath -ldflags "-s" -a -o $name.darwin_arm64_static
+GOARM=7 "${GOBUILD[@]}" $name.linux_armv7_static
+unset GOARM
+
+echo '* macOS x86_64'
+export GOOS=darwin
+GOARCH=amd64 "${GOBUILD[@]}" $name.macos_x86_64_static
+echo '* macOS aarch64'
+GOARCH=arm64 "${GOBUILD[@]}" $name.macos_aarch64_static
+
+echo '* Compressing with upx'
+for f in $name.linux_*_static $name.macos_*_static; do
+  echo "Compressing $f"
+  upx "$f"
+done
+
+echo '* FreeBSD x86_64'
+export GOOS=freebsd
+GOARCH=amd64 "${GOBUILD[@]}" $name.freebsd_x86_64_static
+echo '* FreeBSD aarch64'
+GOARCH=arm64 "${GOBUILD[@]}" $name.freebsd_aarch64_static
+export GOARCH=arm
+echo '* FreeBSD armv6'
+GOARM=6 "${GOBUILD[@]}" $name.freebsd_armv6_static
+echo '* FreeBSD armv7'
+GOARM=7 "${GOBUILD[@]}" $name.freebsd_armv7_static
+
+echo '* NetBSD x86_64'
+export GOOS=netbsd
+GOARCH=amd64 "${GOBUILD[@]}" $name.netbsd_x86_64_static
+echo '* NetBSD aarch64'
+GOARCH=arm64 "${GOBUILD[@]}" $name.netbsd_aarch64_static
+export GOARCH=arm
+echo '* NetBSD armv6'
+GOARM=6 "${GOBUILD[@]}" $name.netbsd_armv6_static
+echo '* NetBSD armv7'
+GOARM=7 "${GOBUILD[@]}" $name.netbsd_armv7_static
+
+# OpenBSD (and Plan9) did not compile: https://github.com/pkg/term/issues/27
 
 # Compress the Linux releases with xz
-for p in linux_amd64 linux_rpi234 linux_amd64_static linux_arm64_static linux_armv6_static linux_armv7_static darwin_arm64_static; do
+for p in linux_x86_64_static linux_aarch64_static linux_armv6_static linux_armv7_static
+do
   echo "Compressing $name-$version.$p.tar.xz"
   mkdir "$name-$version-$p"
   cp ../$name.1 "$name-$version-$p/"
@@ -46,7 +75,11 @@ for p in linux_amd64 linux_rpi234 linux_amd64_static linux_arm64_static linux_ar
 done
 
 # Compress the other tarballs with gz
-for p in macos_amd64 freebsd_amd64 netbsd_amd64; do
+for p in \
+macos_x86_64_static macos_aarch64_static \
+freebsd_x86_64_static freebsd_aarch64_static freebsd_armv6_static freebsd_armv7_static \
+netbsd_x86_64_static netbsd_aarch64_static netbsd_armv6_static netbsd_armv7_static
+do
   echo "Compressing $name-$version.$p.tar.gz"
   mkdir "$name-$version-$p"
   cp ../$name.1 "$name-$version-$p/"
