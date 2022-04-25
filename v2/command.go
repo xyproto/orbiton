0180304c4b6272c5d42cbc0c8f83b6951508b37a
Alexander F. Rødseth <rodseth@gmail.com>
2022-04-25 13:17:38 +0200

    Add a blank line

--- v2/command.go
+++ v2/command.go
@@ -268,6 +268,7 @@ func (e *Editor) CommandMenu(c *vt100.Canvas, tty *vt100.TTY, status *StatusBar,
 				status.Clear(c)
 				status.SetMessage("Debug mode enabled")
 				status.Show(c, e)
+
 				e.debugMode = true
 			})
 		}
