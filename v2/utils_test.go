d4a257b19923c277fb8c6e84fb5674b4908a4034
Alexander F. Rødseth <rodseth@gmail.com>
2022-06-26 12:16:54 +0200

    Add the getFullName utility function

--- v2/utils_test.go
+++ v2/utils_test.go
@@ -1,9 +1,15 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestCapitalizeWords(t *testing.T) {
 	if capitalizeWords("bob john") != "Bob John" {
 		t.Fail()
 	}
 }
+
+// func TestGetFullName(t *testing.T) {
+// 	fmt.Println(getFullName())
+// }
