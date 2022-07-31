47cc2b363caa5c5f9637ddf2095a57733b6cf4d9
Alexander F. Rødseth <rodseth@gmail.com>
2022-07-31 18:29:26 +0200

    Improve the command lookup functionality

--- v2/command.go
+++ v2/command.go
@@ -31,20 +31,33 @@ func (e *Editor) CommandToFunction(c *vt100.Canvas, tty *vt100.TTY, status *Stat
 		}
 	}
 
+	const (
+		help = iota
+		insertdate
+		insertfile
+		quit
+		save
+		savequit
+		savequitclear
+		sortblock
+		sortstrings
+		version
+	)
+
 	// Define args and corresponding functions
-	var commandLookup = map[string]func(){
-		"help": func() { // display an informative status message
+	var commandLookup = map[int]func(){
+		help: func() { // display an informative status message
 			// TODO: Draw the same type of box that is used in debug mode, listing all possible commands
 			status.SetMessageAfterRedraw(":wq, s, save, sq, savequit, q, quit, h, help, sort, v, version")
 		},
-		"insertdate": func() {
+		insertdate: func() {
 			undo.Snapshot(e)
 			// note that if a space is added after the string here, it will be stripped when the command menu disappears
 			dateString := time.Now().Format(time.RFC3339)[:10]
 			e.InsertString(c, dateString)
 			e.addSpace = true
 		},
-		"insertfile": func() {
+		insertfile: func() {
 			undo.Snapshot(e)
 			editedFileDir := filepath.Dir(e.filename)
 			if err := e.InsertFile(c, filepath.Join(editedFileDir, strings.TrimSpace(args[1]))); err != nil {
@@ -53,32 +66,32 @@ func (e *Editor) CommandToFunction(c *vt100.Canvas, tty *vt100.TTY, status *Stat
 				status.Show(c, e)
 			}
 		},
-		"save": func() { // save the current file
+		save: func() { // save the current file
 			e.UserSave(c, tty, status)
 		},
-		"savequit": func() { // save and quit
+		savequit: func() { // save and quit
 			e.UserSave(c, tty, status)
 			e.quit = true
 		},
-		"savequitclear": func() { // save and quit, then clear the screen
+		savequitclear: func() { // save and quit, then clear the screen
 			e.UserSave(c, tty, status)
 			e.quit = true
 			e.clearOnQuit = true
 		},
-		"sortblock": func() { // sort the current block of lines, until the next blank line or EOF
+		sortblock: func() { // sort the current block of lines, until the next blank line or EOF
 			undo.Snapshot(e)
 			e.SortBlock(c, status, bookmark)
 		},
-		"sortstrings": func() { // sort the words on the current line
+		sortstrings: func() { // sort the words on the current line
 			undo.Snapshot(e)
 			e.SortStrings(c, status)
 			e.redraw = true
 			e.redrawCursor = true
 		},
-		"quit": func() { // quit
+		quit: func() { // quit
 			e.quit = true
 		},
-		"version": func() { // display the program name and version as a status message
+		version: func() { // display the program name and version as a status message
 			status.SetMessageAfterRedraw(versionString)
 		},
 	}
@@ -87,28 +100,32 @@ func (e *Editor) CommandToFunction(c *vt100.Canvas, tty *vt100.TTY, status *Stat
 	//       For instance, the save commands could take a filename.
 
 	// Helpful command aliases that can also handle some typos and abbreviations
-	lookupWord := ""
+	var functionID int
 	switch trimmedCommand {
 	case "qs", "byes", "cus", "exitsave", "quitandsave", "quitsave", "qw", "saq", "saveandquit", "saveexit", "saveq", "savequit", "savq", "sq", "wq":
-		lookupWord = "savequit"
+		functionID = savequit
 	case "s", "sa", "sav", "save", "w", "ww":
-		lookupWord = "save"
+		functionID = save
 	case "bye", "cu", "ee", "exit", "q", "qq", "qu", "qui", "quit":
-		lookupWord = "quit"
+		functionID = quit
 	case "h", "he", "hh", "hel", "help":
-		lookupWord = "help"
+		functionID = help
+	case "if", "i", "insertfile", "insert", "insertf":
+		functionID = insertfile
+	case "insertdate", "insertd", "id", "date":
+		functionID = insertdate
 	case "v", "ver", "vv", "version":
-		lookupWord = "version"
+		functionID = version
 	case "sb", "so", "sor", "sort":
-		lookupWord = "sortblock"
+		functionID = sortblock
 	case "sortstrings", "sortw", "sortwords", "sow", "ss", "sw", "sortfields", "sf":
-		lookupWord = "sortstrings"
+		functionID = sortstrings
 	default:
 		return nil, fmt.Errorf("unknown command: %s", args[0])
 	}
 
 	// Return the selected function
-	f, ok := commandLookup[lookupWord]
+	f, ok := commandLookup[functionID]
 	if !ok {
 		return nil, fmt.Errorf("implementation missing for command: %s", args[0])
 	}
