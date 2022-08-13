d2c03afdb1cf7f1b230f6f30ffcf81066c3b0f56
Alexander F. Rødseth <rodseth@gmail.com>
2022-08-13 02:31:09 +0200

    Improve the syntax highlighting for Go

--- v2/syntax.go
+++ v2/syntax.go
@@ -94,7 +94,7 @@ func adjustSyntaxHighlightingKeywords(m mode.Mode) {
 		addKeywords = erlangWords
 	case mode.Go:
 		addKeywords = []string{"defer", "error", "fallthrough", "go", "print", "println", "range", "rune", "string", "uint", "uint8", "uint16", "uint32", "uint64"}
-		delKeywords = []string{"None", "assert", "auto", "build", "char", "def", "def", "del", "die", "done", "end", "fi", "final", "finally", "fn", "from", "get", "in", "include", "is", "last", "let", "match", "mut", "next", "no", "pass", "redo", "rescue", "ret", "retry", "set", "template", "then", "this", "when", "where", "while", "yes"}
+		delKeywords = []string{"False", "None", "True", "assert", "auto", "build", "char", "def", "def", "del", "die", "done", "end", "fi", "final", "finally", "fn", "from", "get", "in", "include", "is", "last", "let", "match", "mut", "next", "no", "pass", "redo", "rescue", "ret", "retry", "set", "template", "then", "this", "when", "where", "while", "yes"}
 	case mode.HIDL:
 		clearKeywords()
 		addKeywords = hidlWords
