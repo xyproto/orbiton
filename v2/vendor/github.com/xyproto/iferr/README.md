94341ed198f2f97525f2b43b5c9cb4bd1a6b7039
Alexander F. Rødseth <rodseth@gmail.com>
2022-08-16 20:34:35 +0200

    Update dependencies

---
+++ v2/vendor/github.com/xyproto/iferr/README.md
@@ -0,0 +1,10 @@
+# IfErr
+
+Generate an `if err != nil {` block given a source file and a line number, that suits the function at that line.
+
+This is a fork of [koron/iferr](https://github.com/koron/iferr), to make it into a Go package that can be used from other Go projects.
+
+# General info
+
+* License: MIT
+* Version: 1.0.0
---
+++ v2/vendor/github.com/xyproto/iferr/iferr.go
@@ -0,0 +1,191 @@
+package iferr
+
+import (
+	"bytes"
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"strings"
+)
+
+const noname = "(no name)"
+
+var isNum = map[string]struct{}{
+	"int":     struct{}{},
+	"int16":   struct{}{},
+	"int32":   struct{}{},
+	"int64":   struct{}{},
+	"uint":    struct{}{},
+	"uint16":  struct{}{},
+	"uint32":  struct{}{},
+	"uint64":  struct{}{},
+	"float":   struct{}{},
+	"float32": struct{}{},
+	"float64": struct{}{},
+}
+
+type Visitor struct {
+	pos token.Pos
+	err error
+	ft  *ast.FuncType
+	fn  string
+}
+
+type Field struct {
+	name string
+}
+
+func (v *Visitor) Visit(node ast.Node) ast.Visitor {
+	if node == nil {
+		return nil
+	}
+	switch x := node.(type) {
+	case *ast.FuncDecl:
+		x, ok := node.(*ast.FuncDecl)
+		if !ok {
+			return v
+		}
+		fname := noname
+		if x.Name != nil {
+			fname = x.Name.Name
+		}
+		fname = x.Name.Name
+		if v.pos < x.Pos() || v.pos > x.End() {
+			return nil
+		}
+		if x.Type == nil {
+			return v
+		}
+		v.fn = fname
+		v.ft = x.Type
+		return v
+	case *ast.FuncLit:
+		if x.Type == nil || x.Body == nil {
+			return nil
+		}
+		if v.pos < x.Pos() || v.pos > x.End() {
+			return nil
+		}
+		v.fn = noname
+		v.ft = x.Type
+		return v
+	default:
+		return v
+	}
+}
+
+func ToTypes(fl *ast.FieldList) []ast.Expr {
+	if fl == nil || len(fl.List) == 0 {
+		return nil
+	}
+	types := make([]ast.Expr, 0, len(fl.List))
+	for _, f := range fl.List {
+		types = append(types, f.Type)
+	}
+	return types
+}
+
+func TypeString(x ast.Expr) string {
+	switch t := x.(type) {
+	case *ast.Ident:
+		return t.Name
+	case *ast.SelectorExpr:
+		if _, ok := t.X.(*ast.Ident); ok {
+			return TypeString(t.X) + "." + t.Sel.Name
+		}
+	case *ast.StarExpr:
+		return "*" + TypeString(t.X)
+	case *ast.ArrayType:
+		return "[]" + TypeString(t.Elt)
+	case *ast.InterfaceType:
+		return "interface{}"
+	case *ast.MapType:
+		return "map[" + TypeString(t.Key) + "]" + TypeString(t.Value)
+	case *ast.StructType:
+		return "struct{}"
+	case *ast.ChanType:
+		return "chan " + TypeString(t.Value)
+	default:
+		return ""
+	}
+	return ""
+}
+
+func GetIfErr(types []ast.Expr) (string, error) {
+	if len(types) == 0 {
+		return "if err != nil {\n\treturn\n}\n", nil
+	}
+	var sb strings.Builder
+	sb.WriteString("if err != nil {\n\treturn ")
+	for i, t := range types {
+		if i > 0 {
+			sb.WriteString(", ")
+		}
+		ts := TypeString(t)
+		if ts == "bool" {
+			sb.WriteString(`false`)
+			continue
+		}
+		if ts == "error" {
+			sb.WriteString("err")
+			continue
+		}
+		if ts == "string" {
+			sb.WriteString(`""`)
+			continue
+		}
+		if ts == "interface{}" {
+			sb.WriteString(`nil`)
+			continue
+		}
+		if _, ok := isNum[ts]; ok {
+			sb.WriteString("0")
+			continue
+		}
+		if strings.HasPrefix(ts, "[]") {
+			sb.WriteString("nil")
+			continue
+		}
+		if strings.HasPrefix(ts, "map[") {
+			sb.WriteString("nil")
+			continue
+		}
+		if strings.HasPrefix(ts, "chan ") {
+			sb.WriteString("nil")
+			continue
+		}
+		if strings.HasPrefix(ts, "*") {
+			sb.WriteString("nil")
+			continue
+		}
+		// treat it as an interface when type name has "."
+		if strings.Index(ts, ".") >= 0 {
+			sb.WriteString("nil")
+			continue
+		}
+		// TODO: support more types.
+		sb.WriteString(ts)
+		sb.WriteString("{}")
+	}
+	sb.WriteString("\n}\n")
+	return sb.String(), nil
+}
+
+func IfErr(data []byte, lineIndex int) (string, error) {
+	r := bytes.NewReader(data)
+	file, err := parser.ParseFile(token.NewFileSet(), "iferr.go", r, 0)
+	if err != nil {
+		return "", err
+	}
+	v := &Visitor{pos: token.Pos(lineIndex)}
+	ast.Walk(v, file)
+	if v.err != nil {
+		return "", err
+	}
+	if v.ft == nil {
+		return "", fmt.Errorf("no functions at %d", lineIndex)
+	}
+	types := ToTypes(v.ft.Results)
+	return GetIfErr(types)
+}
