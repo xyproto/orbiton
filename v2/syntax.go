d74206a177359530b344f2d291c1a941c9352414
Alexander F. Rødseth <rodseth@gmail.com>
2022-08-28 10:05:27 +0200

    Improve the syntax highlighting for Rust

--- v2/syntax.go
+++ v2/syntax.go
@@ -140,7 +140,7 @@ func adjustSyntaxHighlightingKeywords(m mode.Mode) {
 	case mode.Garnet, mode.Hare, mode.Jakt, mode.Rust: // Originally only for Rust, split up as needed
 		addKeywords = []string{"String", "assert_eq", "char", "fn", "i16", "i32", "i64", "i8", "impl", "loop", "mod", "out", "panic", "u16", "u32", "u64", "u8", "usize"}
 		// "as" and "mut" are treated as special cases in the syntax package
-		delKeywords = []string{"as", "build", "byte", "done", "get", "int", "int16", "int32", "int64", "last", "map", "mut", "next", "pass", "print", "uint16", "uint32", "uint64", "var"}
+		delKeywords = []string{"as", "build", "byte", "done", "foreach", "get", "int", "int16", "int32", "int64", "last", "map", "mut", "next", "pass", "print", "uint16", "uint32", "uint64", "var"}
 		if m != mode.Garnet {
 			delKeywords = append(delKeywords, "end")
 		}
