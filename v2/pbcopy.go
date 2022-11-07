ced4ddf2d3b0c9425d129f37d8437101fc501906
Alexander F. Rødseth <rodseth@gmail.com>
2022-11-07 14:31:46 +0100

    Add the start of pbcopy/pbpaste support for macOS

---
+++ v2/pbcopy.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bytes"
+	"os/exec"
+)
+
+func pbcopy(s string) error {
+	cmd := exec.Command("pbcopy")
+	var buf bytes.Buffer
+	buf.WriteString(s)
+	cmd.Stdin = &buf
+	return cmd.Run()
+}
+
+func pbpaste() (string, error) {
+	cmd := exec.Command("pbpaste")
+	var buf bytes.Buffer
+	cmd.Stdout = &buf
+	if err := cmd.Run(); err != nil {
+		return "", nil
+	}
+	return buf.String(), nil
+}
