ccf36972cdebe8df5790c69f74b515ec7f1f16ad
Alexander F. Rødseth <rodseth@gmail.com>
2023-02-25 12:48:24 +0100

    Add a CMake template

--- v2/template.go
+++ v2/template.go
@@ -44,6 +44,11 @@ var templatePrograms = map[mode.Mode]TemplateProgram{
 		6,
 		5,
 	},
+	mode.CMake: {
+		"cmake_minimum_required(VERSION 3.2)\n\nproject(cmake-project-template)\n\nset(CMAKE_CXX_STANDARD 11)\nset(CMAKE_CXX_FLAGS \"${CMAKE_CXX_FLAGS} -std=c++11 -O2\")\n\nset(CMAKE_INSTALL_PREFIX ${PROJECT_SOURCE_DIR})\n\nset(DIVISIBLE_INSTALL_INCLUDE_DIR ${PROJECT_SOURCE_DIR}/include)\nset(DIVISIBLE_INSTALL_BIN_DIR ${PROJECT_SOURCE_DIR}/bin)\nset(DIVISIBLE_INSTALL_LIB_DIR ${PROJECT_SOURCE_DIR}/lib)\n\nset(DIVISION_HEADERS_DIR ${PROJECT_SOURCE_DIR}/src/division)\n\ninclude_directories(${DIVISIBLE_INSTALL_INCLUDE_DIR})\ninclude_directories(${DIVISION_HEADERS_DIR})\n\nadd_subdirectory(src)\nadd_subdirectory(test)\n",
+		8,
+		18,
+	},
 	mode.CS: {
 		"using System;\n\nclass Greeter {\n    public static void Main(string[] args) {\n        Console.WriteLine(\"Hello, World!\");\n    }\n}\n",
 		19,
