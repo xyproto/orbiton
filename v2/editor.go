c161809970d8de508a2e23630bfe6b701c8c32f6
Alexander F. Rødseth <rodseth@gmail.com>
2023-03-26 14:55:46 +0200

    Refactor

--- v2/editor.go
+++ v2/editor.go
@@ -9,7 +9,6 @@ import (
 	"sort"
 	"strconv"
 	"strings"
-	"sync"
 	"time"
 	"unicode"
 	"unicode/utf8"
@@ -384,72 +383,6 @@ func (e *Editor) Load(c *vt100.Canvas, tty *vt100.TTY, fnord FilenameOrData) (st
 type IndexByteLine struct {
 	index    int
 	byteLine []byte
-}
-
-func (e *Editor) LoadByteLine(ib IndexByteLine, eMut, tcMut *sync.RWMutex, tabIndentCounter, numLines *int, wg *sync.WaitGroup) {
-	// Require at least two bytes. Ignore lines with a single tab indentation or a single space
-	if len(ib.byteLine) > 2 {
-		var first byte = ib.byteLine[0]
-		if first == '\t' {
-			tcMut.Lock()
-			*tabIndentCounter++ // a tab indentation counts like a positive tab indentation
-			tcMut.Unlock()
-		} else if first == ' ' && ib.byteLine[1] == ' ' { // assume that two spaces is the smallest space indentation
-			tcMut.Lock()
-			*tabIndentCounter-- // a space indentation counts like a negative tab indentation
-			tcMut.Unlock()
-		}
-	}
-	eMut.Lock()
-	e.lines[ib.index] = []rune(string(ib.byteLine))
-	*numLines++
-	eMut.Unlock()
-	wg.Done()
-}
-
-// LoadBytes replaces the current editor contents with the given bytes
-func (e *Editor) LoadBytes(data []byte) {
-	// Prepare an empty map to load the lines into
-	e.Clear()
-	e.lines = make(map[int][]rune, 0)
-
-	var (
-		// Split the bytes into lines
-		byteLines = bytes.Split(data, []byte{'\n'})
-
-		// Place the lines into the editor, while counting tab indentations vs space indentations
-		tabIndentCounter int
-
-		// Count the number of lines as the lines are being processed
-		numLines int
-
-		// Mutex for the editor lines and the numLines counter
-		eMut sync.RWMutex
-
-		// Mutex for the tabIndentCounter
-		tcMut sync.RWMutex
-	)
-
-	var wg sync.WaitGroup
-	for index, byteLine := range byteLines {
-		wg.Add(1)
-		go e.LoadByteLine(IndexByteLine{index, byteLine}, &eMut, &tcMut, &tabIndentCounter, &numLines, &wg)
-	}
-	wg.Wait()
-
-	// If the last line is empty, delete it
-	if numLines > 0 && len(e.lines[numLines-1]) == 0 {
-		delete(e.lines, numLines-1)
-	}
-
-	if detectedTabs := tabIndentCounter > 0; detectedTabs {
-		// Check if there were more tab indentations than space indentations
-		e.detectedTabs = &detectedTabs
-		e.indentation.Spaces = !detectedTabs
-	}
-
-	// Mark the editor contents as "changed"
-	e.changed = true
 }
 
 // PrepareEmpty prepares an empty textual representation of a given filename.
--- v2/readfile.go
+++ v2/readfile.go
@@ -121,3 +121,70 @@ func (e *Editor) ReadFileAndProcessLines(filename string) error {
 
 	return nil
 }
+
+// LoadByteLine loads a single byte line
+func (e *Editor) LoadByteLine(ib IndexByteLine, eMut, tcMut *sync.RWMutex, tabIndentCounter, numLines *int, wg *sync.WaitGroup) {
+	// Require at least two bytes. Ignore lines with a single tab indentation or a single space
+	if len(ib.byteLine) > 2 {
+		var first byte = ib.byteLine[0]
+		if first == '\t' {
+			tcMut.Lock()
+			*tabIndentCounter++ // a tab indentation counts like a positive tab indentation
+			tcMut.Unlock()
+		} else if first == ' ' && ib.byteLine[1] == ' ' { // assume that two spaces is the smallest space indentation
+			tcMut.Lock()
+			*tabIndentCounter-- // a space indentation counts like a negative tab indentation
+			tcMut.Unlock()
+		}
+	}
+	eMut.Lock()
+	e.lines[ib.index] = []rune(string(ib.byteLine))
+	*numLines++
+	eMut.Unlock()
+	wg.Done()
+}
+
+// LoadBytes replaces the current editor contents with the given bytes
+func (e *Editor) LoadBytes(data []byte) {
+	// Prepare an empty map to load the lines into
+	e.Clear()
+	e.lines = make(map[int][]rune, 0)
+
+	var (
+		// Split the bytes into lines
+		byteLines = bytes.Split(data, []byte{'\n'})
+
+		// Place the lines into the editor, while counting tab indentations vs space indentations
+		tabIndentCounter int
+
+		// Count the number of lines as the lines are being processed
+		numLines int
+
+		// Mutex for the editor lines and the numLines counter
+		eMut sync.RWMutex
+
+		// Mutex for the tabIndentCounter
+		tcMut sync.RWMutex
+	)
+
+	var wg sync.WaitGroup
+	for index, byteLine := range byteLines {
+		wg.Add(1)
+		go e.LoadByteLine(IndexByteLine{index, byteLine}, &eMut, &tcMut, &tabIndentCounter, &numLines, &wg)
+	}
+	wg.Wait()
+
+	// If the last line is empty, delete it
+	if numLines > 0 && len(e.lines[numLines-1]) == 0 {
+		delete(e.lines, numLines-1)
+	}
+
+	if detectedTabs := tabIndentCounter > 0; detectedTabs {
+		// Check if there were more tab indentations than space indentations
+		e.detectedTabs = &detectedTabs
+		e.indentation.Spaces = !detectedTabs
+	}
+
+	// Mark the editor contents as "changed"
+	e.changed = true
+}
