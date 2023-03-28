71175210f35ca0c511a096ad1368248c929e3a37
Alexander F. Rødseth <rodseth@gmail.com>
2023-03-28 13:56:16 +0200

    Add an experimental/debug feature

--- v2/redraw.go
+++ v2/redraw.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/xyproto/env/v2"
 	"github.com/xyproto/vt100"
 )
 
@@ -23,6 +24,9 @@ func (e *Editor) FullResetRedraw(c *vt100.Canvas, status *StatusBar, drawLines b
 	newC := vt100.NewCanvas()
 	newC.ShowCursor()
 	vt100.EchoOff()
+
+	newC.SetLineWrap(env.Bool("O_LW"))
+
 	w := int(newC.Width())
 
 	resizeMut.Unlock()
