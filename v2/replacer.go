eff48421b71df766052ac7ca476cd42e78c307e7
Alexander F. Rødseth <rodseth@gmail.com>
2023-03-28 16:35:43 +0200

    Remove an unused opinionated replacer

--- v2/replacer.go
+++ v2/replacer.go
@@ -2,26 +2,11 @@ package main
 
 import (
 	"strings"
-
-	"go4.org/bytereplacer"
 )
 
 // opinionatedStringReplacer is a Replacer that can be used for fixing
 // nonbreaking spaces, annoying tildes, \r\n and \r
 var opinionatedStringReplacer = strings.NewReplacer(
-	// Replace non-breaking space with regular space
-	string([]byte{0xc2, 0xa0}), string([]byte{0x20}),
-	// Fix annoying tilde
-	string([]byte{0xcc, 0x88}), string([]byte{'~'}),
-	// Replace DOS line endings with UNIX line endings
-	string([]byte{'\r', '\n'}), string([]byte{'\n'}),
-	// Replace any remaining \r characters with \n
-	string([]byte{'\r'}), string([]byte{'\n'}),
-)
-
-// opinionatedByteReplacer is a bytereplacer that can be used for fixing
-// nonbreaking spaces, annoying tildes, \r\n and \r
-var opinionatedByteReplacer = bytereplacer.New(
 	// Replace non-breaking space with regular space
 	string([]byte{0xc2, 0xa0}), string([]byte{0x20}),
 	// Fix annoying tilde
