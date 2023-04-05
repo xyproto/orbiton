4ed73488e2927c713641ad960923943bab5f221d
Alexander F. Rødseth <rodseth@gmail.com>
2023-04-05 15:31:42 +0200

    Let version.sh also work on macOS

--- version.sh
+++ version.sh
@@ -4,7 +4,7 @@
 #
 
 # The current version goes here, as the default value
-VERSION=${1:-'2.60.4'}
+VERSION=${1:-'2.60.5'}
 
 if [ -z "$1" ]; then
   echo "The current version is $VERSION, pass the new version as the first argument if you wish to change it"
@@ -15,5 +15,5 @@ echo "Setting the version to $VERSION"
 
 # Update the date and version in the man page, README.md file and also this script
 d=$(LC_ALL=C date +'%d %b %Y')
-sed -r -i "s/\"[0-9]* [A-Z][a-z]* [0-9]*\"/\"$d\"/g" o.1
-sed -r -i "s/2\.[[:digit:]]+\.[[:digit:]]+/$VERSION/g" o.1 README.md "$0" v2/main.go aur.sh
+sed -E -i '' "s/\"[0-9]* [A-Z][a-z]* [0-9]*\"/\"$d\"/g" o.1
+sed -E -i '' "s/2\.[[:digit:]]+\.[[:digit:]]+/$VERSION/g" o.1 README.md "$0" v2/main.go aur.sh
