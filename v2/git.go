929bef15154a0256edef47b140f4278c703a152a
Alexander F. Rødseth <rodseth@gmail.com>
2023-04-07 23:57:47 +0200

    Refactor

--- v2/git.go
+++ v2/git.go
@@ -8,43 +8,39 @@ import (
 
 var gitRebasePrefixes = []string{"p", "pick", "f", "fixup", "r", "reword", "d", "drop", "e", "edit", "s", "squash", "x", "exec", "b", "break", "l", "label", "t", "reset", "m", "merge"}
 
+// getNextInCycle returns the next element in the cycle, given the current element and the cycle
+func getNextInCycle(current string, cycle []string) string {
+	for i, w := range cycle {
+		if current == w {
+			if i+1 < len(cycle) {
+				return cycle[i+1]
+			}
+			return cycle[0]
+		}
+	}
+	return ""
+}
+
 // nextGitRebaseKeyword will use the first word in the given line,
 // and replace it with the next git rebase keyword (as ordered in gitRebasePrefixes)
 func nextGitRebaseKeyword(line string) string {
-	var (
-		cycle1 = filterS(gitRebasePrefixes, func(s string) bool { return len(s) > 1 })
-		cycle2 = filterS(gitRebasePrefixes, func(s string) bool { return len(s) == 1 })
-		first  = strings.Fields(line)[0]
-		next   = ""
-	)
-	// Check if the word is in cycle1, then set "next" to the next one in the cycle
-	for i, w := range cycle1 {
-		if first == w {
-			if i+1 < len(cycle1) {
-				next = cycle1[i+1]
-				break
-			}
-			next = cycle1[0]
-		}
-	}
+	cycle1 := filterS(gitRebasePrefixes, func(s string) bool { return len(s) > 1 })
+	cycle2 := filterS(gitRebasePrefixes, func(s string) bool { return len(s) == 1 })
+
+	firstWord := strings.Fields(line)[0]
+	next := getNextInCycle(firstWord, cycle1)
+
 	if next == "" {
-		// Check if the word is in cycle2, then set "next" to the next one in the cycle
-		for i, w := range cycle2 {
-			if first == w {
-				if i+1 < len(cycle2) {
-					next = cycle2[i+1]
-					break
-				}
-				next = cycle2[0]
-			}
-		}
+		next = getNextInCycle(firstWord, cycle2)
 	}
+
 	if next == "" {
 		// Return the line as it is, no git rebase keyword found
 		return line
 	}
+
 	// Return the line with the keyword replaced with the next one in cycle1 or cycle2
-	return strings.Replace(line, first, next, 1)
+	return strings.Replace(line, firstWord, next, 1)
 }
 
 func (e *Editor) gitHighlight(line string) string {
