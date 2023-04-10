d28faa7b56403a208a20875188b5014b25aae099
Alexander F. Rødseth <rodseth@gmail.com>
2023-04-11 00:13:15 +0200

    Update the version script

--- version.sh
+++ version.sh
@@ -4,7 +4,7 @@
 #
 
 # The current version goes here, as the default value
-VERSION=${1:-'2.60.5'}
+VERSION=${1:-'2.60.6'}
 
 if [ -z "$1" ]; then
   echo "The current version is $VERSION, pass the new version as the first argument if you wish to change it"
@@ -15,5 +15,11 @@ echo "Setting the version to $VERSION"
 
 # Update the date and version in the man page, README.md file and also this script
 d=$(LC_ALL=C date +'%d %b %Y')
-sed -E -i '' "s/\"[0-9]* [A-Z][a-z]* [0-9]*\"/\"$d\"/g" o.1
-sed -E -i '' "s/2\.[[:digit:]]+\.[[:digit:]]+/$VERSION/g" o.1 README.md "$0" v2/main.go aur.sh
+
+# macOS
+sed -E -i '' "s/\"[0-9]* [A-Z][a-z]* [0-9]*\"/\"$d\"/g" o.1 || true
+sed -E -i '' "s/2\.[[:digit:]]+\.[[:digit:]]+/$VERSION/g" o.1 README.md "$0" v2/main.go aur.sh || true
+
+# Linux
+sed -r -i "s/\"[0-9]* [A-Z][a-z]* [0-9]*\"/\"$d\"/g" o.1 || true
+sed -r -i "s/2\.[[:digit:]]+\.[[:digit:]]+/$VERSION/g" o.1 README.md "$0" v2/main.go aur.sh || true
