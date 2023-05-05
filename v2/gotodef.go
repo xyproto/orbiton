9299307fd89bc1ddd91c6ab34f5641f0ebf58180
Alexander F. Rødseth <rodseth@gmail.com>
2023-05-05 15:46:22 +0200

    Add "jump to definition" for functions, for the same file

--- v2/gotodef.go
+++ v2/gotodef.go
@@ -3,24 +3,21 @@ package main
 import (
 	"strings"
 
-	"github.com/xyproto/mode"
 	"github.com/xyproto/syntax"
 	"github.com/xyproto/vt100"
 )
 
-// canGoToDefinition checks if the "go to definition" feature has been implemented for this file mode yet
-func (e *Editor) canGoToDefinition() bool {
-	// TODO: Add all modes that makes sense
-	return e.mode == mode.Go
-}
-
 // GoToDefinition tries to find the definition of the given string, saves the current location and jumps to the location of the definition.
 // Returns true if it was possible to go to the definition.
 func (e *Editor) GoToDefinition(c *vt100.Canvas, status *StatusBar) bool {
+	// FuncPrefix may return strings with a leading or trailing blank
+	funcPrefix := e.FuncPrefix()
+
 	// Can this language / editor mode support this?
-	if !e.canGoToDefinition() {
+	if funcPrefix == "" {
 		return false
 	}
+
 	// Do we have a word under the cursor? No need to trim it at this point.
 	word := e.WordAtCursor()
 	if word == "" {
@@ -48,12 +45,13 @@ func (e *Editor) GoToDefinition(c *vt100.Canvas, status *StatusBar) bool {
 
 	status.ClearAll(c)
 
-	s := "func " + word
+	// The search string we will use for searching for functions within this file
+	s := funcPrefix + word
 
 	// Go to defintion, but only of functions defined within the same Go file, for now
 	e.SetSearchTerm(c, status, s)
 
-	// Backward search form the current location
+	// Backward search from the current location
 	startIndex := e.DataY()
 	stopIndex := LineIndex(0)
 	foundX, foundY := e.backwardSearch(startIndex, stopIndex)
@@ -70,12 +68,16 @@ func (e *Editor) GoToDefinition(c *vt100.Canvas, status *StatusBar) bool {
 		tabs := strings.Count(e.Line(foundY), "\t")
 		e.pos.sx = foundX + (tabs * (e.indentation.PerTab - 1))
 		e.HorizontalScrollIfNeeded(c)
-	}
+	} else {
 
-	// Center and prepare to redraw
-	e.Center(c)
-	e.redraw = true
-	e.redrawCursor = e.redraw
+		// Clear the current search
+		//e.SetSearchTerm(c, status, "")
+
+		// Center and prepare to redraw
+		e.Center(c)
+		e.redraw = true
+		e.redrawCursor = e.redraw
+	}
 
 	//status.SetMessage("Jumped to " + s)
 	//status.Show(c, e)
