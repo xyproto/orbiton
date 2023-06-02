6f6815b881d1e36c9c9efe01e5782d7806746c14
Alexander F. Rødseth <rodseth@gmail.com>
2023-06-02 16:40:03 +0200

    Initial commit of the Orbiton web page

---
+++ web/README.md
@@ -0,0 +1,13 @@
+# orbiton.zip web site
+
+`index.html` is currently just a placeholder.
+
+## Run locally
+
+    ./docker.sh
+
+Then open [`http://localhost:8080`](http://localhost:8080) in a browser.
+
+## Push to The Cloud
+
+    ./docker.sh buildpush
---
+++ web/docker.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+DOCKER_TAG='0.2.0'
+
+# Google Cloud
+PROJECT_ID='personal-306017'
+
+# Define variables for a few color codes, if NO_COLOR is unset
+if [ -z "$NO_COLOR" ]; then
+  # dark red
+  RED='\033[0;31m'
+  # light green
+  GREEN='\033[1;32m'
+  # light blue
+  BLUE='\033[1;34m'
+  # no color
+  OFF='\033[0m'
+fi
+
+build() {
+  # Build a docker image
+  docker build . -f Dockerfile -t "orbiton.zip:3000/orbiton:$DOCKER_TAG"
+}
+
+launch() {
+  # Run the docker image
+  docker run \
+    -i \
+    -t \
+    --rm \
+    -p 8080 \
+    --name orbiton_dev \
+    --network host \
+    "orbiton.zip:3000/orbiton:$DOCKER_TAG"
+}
+
+main() {
+  # Tag the resulting docker image, then either push or run
+  case $1 in
+    build)
+      # Just build
+      build
+      ;;
+    buildpush)
+      # Build, tag and push
+      build && \
+        docker tag "orbiton.zip:3000/orbiton:$DOCKER_TAG" \
+          "europe-north1-docker.pkg.dev/personal-306017/homepage/orbiton:$DOCKER_TAG" && \
+        docker push europe-north1-docker.pkg.dev/personal-306017/homepage/orbiton:$DOCKER_TAG
+        ;;
+    *)
+      # Build the docker image and start a container
+      build && launch
+  esac
+}
+
+main "$@"
