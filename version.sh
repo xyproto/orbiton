caf39f74332db4975d4e5851d79997da875d8703
Alexander F. Rødseth <rodseth@gmail.com>
2023-07-06 23:27:54 +0200

    New patch release

--- version.sh
+++ version.sh
@@ -4,7 +4,7 @@
 #
 
 # The current version goes here, as the default value
-VERSION=${1:-'2.62.5'}
+VERSION=${1:-'2.62.6'}
 
 if [ -z "$1" ]; then
   echo "The current version is $VERSION, pass the new version as the first argument if you wish to change it"
