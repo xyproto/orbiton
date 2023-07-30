3aa90346c4dd5ea593d28b98c22abff1e12f0d47
Alexander F. Rødseth <rodseth@gmail.com>
2023-07-30 02:35:23 +0200

    Remove a comment

--- v2/help.go
+++ v2/help.go
@@ -6,7 +6,6 @@ import (
 
 // HelpMessage tries to show a friendly help message to the user.
 func (e *Editor) HelpMessage(c *vt100.Canvas, status *StatusBar) {
-	// TODO: Implement a better help message, that covers more of the screen
 	status.ClearAll(c)
 	status.SetMessage("Press ctrl-q to quit or ctrl-o to show the menu. Use the --help flag for more help.")
 	status.Show(c, e)
