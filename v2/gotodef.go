830847c032f6d3a0e8f4767a0929f03bb4085bc2
Alexander F. Rødseth <rodseth@gmail.com>
2023-08-11 15:26:13 +0200

    Change the search order when jumping to definition (local first)

--- v2/gotodef.go
+++ v2/gotodef.go
@@ -14,6 +14,7 @@ var backFunctions []func()
 // GoToDefinition tries to find the definition of the given string, saves the current location and jumps to the location of the definition.
 // Returns true if it was possible to go to the definition.
 // This function is currently very experimental and may only work for a few languages, and for a few definitions!
+// TODO: Parse some programming langages before jumping.
 func (e *Editor) GoToDefinition(tty *vt100.TTY, c *vt100.Canvas, status *StatusBar) bool {
 	// FuncPrefix may return strings with a leading or trailing blank
 	funcPrefix := e.FuncPrefix()
@@ -35,6 +36,47 @@ func (e *Editor) GoToDefinition(tty *vt100.TTY, c *vt100.Canvas, status *StatusB
 			// Don't go to the definition of keywords
 			return false
 		}
+	}
+
+	// The search string we will use for searching for functions within this file
+	s := funcPrefix + word
+
+	// Or should one search for a method instead?
+	if strings.Contains(word, ".") {
+		fields := strings.SplitN(word, ".", 2)
+		methodName := fields[1]
+		if strings.Contains(methodName, "[") {
+			fields := strings.SplitN(methodName, "[", 2)
+			arrayOrMapName := fields[0]
+			// TODO: Also look for const and in "var"-blocks
+			s = "var " + arrayOrMapName
+		} else {
+			s = ") " + methodName + "("
+		}
+	}
+
+	// TODO: Search for variables, constants etc
+	// Go to definition, but only of functions defined within the same Go file, for now
+	e.SetSearchTerm(c, status, s)
+
+	// Backward search from the current location
+	startIndex := e.DataY()
+	stopIndex := LineIndex(0)
+	foundX, foundY := e.backwardSearch(startIndex, stopIndex)
+	if foundY != -1 {
+		// Go to the found match
+		e.redraw, _ = e.GoTo(foundY, c, status)
+		if foundX == -1 {
+			// Center and prepare to redraw
+			e.Center(c)
+			e.redraw = true
+			e.redrawCursor = e.redraw
+			return false
+		}
+		tabs := strings.Count(e.Line(foundY), "\t")
+		e.pos.sx = foundX + (tabs * (e.indentation.PerTab - 1))
+		e.HorizontalScrollIfNeeded(c)
+		return true
 	}
 
 	currentLine := e.CurrentLine()
@@ -126,69 +168,5 @@ func (e *Editor) GoToDefinition(tty *vt100.TTY, c *vt100.Canvas, status *StatusB
 
 	}
 
-	// TODO:
-	// * Implement "go to definition"
-	// * Go to definition should store the current location in a special kind of bookmark (including filename)
-	//   so that another keypress can jump back to where we were.
-	// * Implement a special kind of bookmark which also supports storing the filename.
-
-	//bookmark = e.pos.Copy()
-	//s := "Bookmarked line " + e.LineNumber().String()
-	//status.SetMessage("  " + s + "  ")
-
-	//status.ClearAll(c)
-
-	// The search string we will use for searching for functions within this file
-	s := funcPrefix + word
-
-	// Or should one search for a method instead?
-	if strings.Contains(word, ".") {
-		fields := strings.SplitN(word, ".", 2)
-		methodName := fields[1]
-		if strings.Contains(methodName, "[") {
-			fields := strings.SplitN(methodName, "[", 2)
-			arrayOrMapName := fields[0]
-			// TODO: Also look for const and in "var"-blocks
-			s = "var " + arrayOrMapName
-		} else {
-			s = ") " + methodName + "("
-		}
-	}
-
-	// TODO: Search for variables, constants etc
-
-	// Go to definition, but only of functions defined within the same Go file, for now
-	e.SetSearchTerm(c, status, s)
-
-	// Backward search from the current location
-	startIndex := e.DataY()
-	stopIndex := LineIndex(0)
-	foundX, foundY := e.backwardSearch(startIndex, stopIndex)
-
-	if foundY == -1 {
-		//status.SetMessage("Could not find " + s)
-		//status.Show(c, e)
-		return false
-	}
-
-	// Go to the found match
-	e.redraw, _ = e.GoTo(foundY, c, status)
-	if foundX != -1 {
-		tabs := strings.Count(e.Line(foundY), "\t")
-		e.pos.sx = foundX + (tabs * (e.indentation.PerTab - 1))
-		e.HorizontalScrollIfNeeded(c)
-	} else {
-
-		// Clear the current search
-		//e.SetSearchTerm(c, status, "")
-
-		// Center and prepare to redraw
-		e.Center(c)
-		e.redraw = true
-		e.redrawCursor = e.redraw
-
-		return false
-	}
-
-	return true
+	return false
 }
