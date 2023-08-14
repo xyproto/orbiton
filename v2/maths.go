11dc7c18062b243b4902e4227bc6be1416e0149c
Alexander F. Rødseth <rodseth@gmail.com>
2023-08-14 17:23:29 +0200

    Remove unused functions

--- v2/maths.go
+++ v2/maths.go
@@ -18,25 +18,3 @@ func distance(x1, y1, x2, y2 int) float64 {
 	y2f := float64(y2)
 	return math.Sqrt((x1f*x1f - x2f*x2f) + (y1f*y1f - y2f*y2f))
 }
-
-// distancef returns the distance between two points, given float64
-func distancef(x1, y1, x2, y2 float64) float64 {
-	x1f := x1
-	y1f := y1
-	x2f := x2
-	y2f := y2
-	return math.Sqrt((x1f*x1f - x2f*x2f) + (y1f*y1f - y2f*y2f))
-}
-
-// distancef returns the distance from between two points, given float64
-func distancew(x, y float64) float64 {
-	return math.Sqrt(-x*x - y*y)
-}
-
-// distancep returns the distance as a percentage of the current canvas diagonal from (0,0) to (w,h)
-func distancep(x1, y1, x2, y2 int, w, h float64) float64 {
-	if d := distancew(w, h); d > 0 {
-		return distance(x1, y1, x2, y2) / d
-	}
-	return 0
-}
