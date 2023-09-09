48c81f419a64e1144fbd2d2403ac3194bf9724ac
Alexander F. Rødseth <rodseth@gmail.com>
2023-09-09 10:16:27 +0200

    Let the profiling functionality use pflag too

--- v2/trace.go
+++ v2/trace.go
@@ -3,32 +3,32 @@
 package main
 
 import (
-	"flag"
 	"log"
 	"os"
 	"runtime"
 	"runtime/pprof"
 
 	"github.com/felixge/fgtrace"
+	"github.com/spf13/pflag"
 )
 
 var (
-	cpuProfileFilename *string
-	memProfileFilename *string
-	fgtraceFilename    *string
+	cpuProfileFilename string
+	memProfileFilename string
+	fgtraceFilename    string
 	cpuProfileFile     os.File
 )
 
 func init() {
-	cpuProfileFilename = flag.String("cpuprofile", "", "write CPU profile to `file`")
-	memProfileFilename = flag.String("memprofile", "", "write memory profile to `file`")
-	fgtraceFilename = flag.String("fgtrace", "", "write fgtrace to `file`")
+	pflag.StringVarP(&cpuProfileFilename, "cpuprofile", "u", "", "write CPU profile to `file`")
+	pflag.StringVarP(&memProfileFilename, "memprofile", "e", "", "write memory profile to `file`")
+	pflag.StringVarP(&fgtraceFilename, "fgtrace", "g", "", "write fgtrace to `file`")
 }
 
 func traceStart() {
 	// Output CPU profile information, if a filename is given
-	if *cpuProfileFilename != "" {
-		cpuProfileFile, err := os.Create(*cpuProfileFilename)
+	if cpuProfileFilename != "" {
+		cpuProfileFile, err := os.Create(cpuProfileFilename)
 		if err != nil {
 			log.Fatal("could not create CPU profile: ", err)
 		}
@@ -40,15 +40,15 @@ func traceStart() {
 		}
 	}
 
-	if *fgtraceFilename != "" {
-		defer fgtrace.Config{Dst: fgtrace.File(*fgtraceFilename)}.Trace().Stop()
+	if fgtraceFilename != "" {
+		defer fgtrace.Config{Dst: fgtrace.File(fgtraceFilename)}.Trace().Stop()
 	}
 }
 
 func traceComplete() {
 	// Output memory profile information, if a filename is given
-	if *memProfileFilename != "" {
-		f, err := os.Create(*memProfileFilename)
+	if memProfileFilename != "" {
+		f, err := os.Create(memProfileFilename)
 		if err != nil {
 			log.Fatal("could not create memory profile: ", err)
 			logf("could not create memory profile: %v\n", err)
@@ -60,7 +60,7 @@ func traceComplete() {
 			logf("could not write to memory profile: %v\n", err)
 		}
 	}
-	if *cpuProfileFilename != "" {
+	if cpuProfileFilename != "" {
 		pprof.StopCPUProfile()
 		cpuProfileFile.Close()
 	}
