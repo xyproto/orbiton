f6a6e08b6a6e40b2b7ca3eebdadae60ccc8ba81a
Alexander F. Rødseth <rodseth@gmail.com>
2023-09-16 19:54:55 +0200

    Add a test that fails

--- v2/spellcheck_test.go
+++ v2/spellcheck_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"strings"
 	"testing"
 )
 
@@ -51,5 +52,52 @@ func TestSearchForTypo(t *testing.T) {
 	// Assuming the correction for "helllo" is "hello"
 	if corrected != "hello" {
 		t.Fatalf("Expected corrected word \"hello\" but got \"%s\".", corrected)
+	}
+}
+
+func TestSpellCheckerTraining(t *testing.T) {
+	sc, _ := NewSpellChecker()
+	initialModel := sc.fuzzyModel
+	sc.Train(true) // force re-train
+	if sc.fuzzyModel == initialModel {
+		t.Fatal("Fuzzy model did not retrain as expected.")
+	}
+}
+
+func TestDefaultSpellCheckerInitialization(t *testing.T) {
+	if spellChecker == nil {
+		t.Fatal("Default spell checker is not initialized.")
+	}
+	if len(spellChecker.correctWords) == 0 {
+		t.Fatal("Default spell checker has no correct words loaded.")
+	}
+}
+
+func TestMultipleTypos(t *testing.T) {
+	e := NewSimpleEditor(80)
+	e.InsertStringAndMove(nil, "Thiss is a testt sentencee with multiplee typos.")
+
+	expectedTypos := []string{"Thiss", "testt", "sentencee", "multiplee"}
+	for _, expectedTypo := range expectedTypos {
+		typo, _, err := e.SearchForTypo()
+		if err != nil {
+			t.Fatalf("Error encountered when searching for typo: %s", err)
+		}
+		if typo != expectedTypo {
+			t.Fatalf("Expected typo \"%s\" but got \"%s\".", expectedTypo, typo)
+		}
+		// Simulate correcting the typo to continue the loop
+		e.InsertStringAndMove(nil, strings.Repeat("\b", len(typo)))
+		e.InsertStringAndMove(nil, " ")
+	}
+}
+
+func TestNoTypo(t *testing.T) {
+	e := NewSimpleEditor(80)
+	e.InsertStringAndMove(nil, "This is a correct sentence.")
+
+	_, _, err := e.SearchForTypo()
+	if err != errFoundNoTypos {
+		t.Fatal("Expected 'errFoundNoTypos' when searching for typo in a correct sentence.")
 	}
 }
