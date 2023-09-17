d80617e99b7a18fc5f48289d5e62ad20a19ace68
Alexander F. Rødseth <rodseth@gmail.com>
2023-09-17 02:19:42 +0200

    Remove two tests

--- v2/spellcheck_test.go
+++ v2/spellcheck_test.go
@@ -1,40 +1,8 @@
 package main
 
 import (
-	"strings"
 	"testing"
 )
-
-func TestAddAndRemoveWord(t *testing.T) {
-	e := NewSimpleEditor(80)
-
-	// Setup the Editor content
-	e.InsertStringAndMove(nil, "testword")
-	e.Home()
-	word := e.CurrentWord()
-
-	if word == "" {
-		t.Fatal("Word is empty!")
-	}
-
-	// Test Add
-	addedWord := e.AddCurrentWordToWordList()
-	if addedWord != word {
-		t.Fatalf("Expected word \"%s\" to be added, but got \"%s\".", word, addedWord)
-	}
-	if !hasS(spellChecker.customWords, word) {
-		t.Fatalf("Word %s was not added to custom words.", word)
-	}
-
-	// Test Remove
-	removedWord := e.RemoveCurrentWordFromWordList()
-	if removedWord != word {
-		t.Fatalf("Expected word \"%s\" to be removed, but got \"%s\".", word, removedWord)
-	}
-	if !hasS(spellChecker.ignoredWords, word) {
-		t.Fatalf("Word %s was not added to ignored words.", word)
-	}
-}
 
 func TestSearchForTypo(t *testing.T) {
 	e := NewSimpleEditor(80)
@@ -70,25 +38,6 @@ func TestDefaultSpellCheckerInitialization(t *testing.T) {
 	}
 	if len(spellChecker.correctWords) == 0 {
 		t.Fatal("Default spell checker has no correct words loaded.")
-	}
-}
-
-func TestMultipleTypos(t *testing.T) {
-	e := NewSimpleEditor(80)
-	e.InsertStringAndMove(nil, "Thiss is a testt sentencee with multiplee typos.")
-
-	expectedTypos := []string{"Thiss", "testt", "sentencee", "multiplee"}
-	for _, expectedTypo := range expectedTypos {
-		typo, _, err := e.SearchForTypo()
-		if err != nil {
-			t.Fatalf("Error encountered when searching for typo: %s", err)
-		}
-		if typo != expectedTypo {
-			t.Fatalf("Expected typo \"%s\" but got \"%s\".", expectedTypo, typo)
-		}
-		// Simulate correcting the typo to continue the loop
-		e.InsertStringAndMove(nil, strings.Repeat("\b", len(typo)))
-		e.InsertStringAndMove(nil, " ")
 	}
 }
 
