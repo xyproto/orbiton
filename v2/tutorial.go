9861138bf1e8812bf60bf0acae8ae598316802bc
Alexander F. Rødseth <rodseth@gmail.com>
2023-09-23 22:44:10 +0200

    Add a placeholder for the tutorial

---
+++ v2/tutorial.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/xyproto/vt100"
+)
+
+func Tutorial(c *vt100.Canvas, e *Editor, status *StatusBar) {
+	status.SetMessageAfterRedraw("TUTORIAL: NOT YET IMPLEMENTED")
+}
