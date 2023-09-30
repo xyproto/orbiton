56cde9130e977d875d291ca6982226cc5189cb19
Alexander F. Rødseth <rodseth@gmail.com>
2023-09-30 15:48:54 +0200

    Remove two tests

--- v2/process_test.go
+++ v2/process_test.go
@@ -13,23 +13,3 @@ func TestParentIsMan(t *testing.T) {
 		t.Logf("Parent process is not 'man'.")
 	}
 }
-
-func TestParentCommand(t *testing.T) {
-	cmd := parentCommand()
-	if cmd == "" {
-		t.Errorf("Failed to get parent command")
-	} else {
-		t.Logf("Parent command: %s", cmd)
-	}
-}
-
-func TestTerminalEmulator(t *testing.T) {
-	term, err := terminalEmulator()
-	if err != nil {
-		t.Errorf("Error: %v", err)
-	} else if term == "" {
-		t.Logf("Terminal emulator not found")
-	} else {
-		t.Logf("Terminal emulator: %s", term)
-	}
-}
