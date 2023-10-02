f2cd11cef73d1255e3744655b9472c7299204b52
Alexander F. Rødseth <rodseth@gmail.com>
2023-10-02 10:45:35 +0200

    Let "osudo" act like "EDITOR=o visudo"

--- v2/main.go
+++ v2/main.go
@@ -13,6 +13,7 @@ import (
 	"syscall"
 
 	"github.com/spf13/pflag"
+	"github.com/xyproto/env/v2"
 	"github.com/xyproto/files"
 	"github.com/xyproto/vt100"
 )
@@ -177,6 +178,25 @@ func main() {
 				os.Exit(1)
 			}
 			return
+		}
+		if executableName == "osudo" {
+			// Build the environment with the EDITOR variable set to "o"
+			env := append(env.Environ(), "EDITOR=o")
+
+			// Get the path to the visudo executable
+			visudoPath := files.Which("visudo")
+			if visudoPath != "" { // success
+				// Replace the current process with visudo
+				if err := syscall.Exec(visudoPath, []string{"visudo"}, env); err != nil {
+					// Could not exec visudo
+					fmt.Fprintln(os.Stderr, err)
+					quitMut.Lock()
+					defer quitMut.Unlock()
+					os.Exit(1)
+				}
+				// No need to return here, because syscall.Exec replaces the current process
+			}
+			// If visudo was not found, start the editor as normal
 		}
 	}
 
