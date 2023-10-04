0e2119b163a8e5735d99f0d9bb3ec8fc8a40a3dd
Alexander F. Rødseth <rodseth@gmail.com>
2023-10-04 10:45:35 +0200

    Update dependencies

--- v2/vendor/github.com/xyproto/env/v2/session.go
+++ v2/vendor/github.com/xyproto/env/v2/session.go
@@ -5,11 +5,17 @@ import "strings"
 // WaylandSession returns true of XDG_SESSION_TYPE is "wayland" or if
 // DESKTOP_SESSION contains "wayland".
 func WaylandSession() bool {
-	return Str("XDG_SESSION_TYPE") == "wayland" || strings.Contains(Str("DESKTOP_SESSION"), "wayland")
+	return Str("XDG_SESSION_TYPE") == "wayland" || Has("WAYLAND_DISPLAY") || strings.Contains(Str("DESKTOP_SESSION"), "wayland")
 }
 
-// XSession returns true if DISPLAY is set and WaylandSession() returns false.
+// XSession returns true if DISPLAY is set.
+// X being available is not neccessarily in opposition to Wayland running.
 func XSession() bool {
+	return Has("DISPLAY")
+}
+
+// OnlyXSession returns true if DISPLAY is set and WaylandSession() is false.
+func OnlyXSession() bool {
 	return Has("DISPLAY") && !WaylandSession()
 }
 
