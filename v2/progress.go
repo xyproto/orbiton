b0489fe799cde8508b9c4db6c9586a095ff0c920
Alexander F. Rødseth <rodseth@gmail.com>
2023-12-04 13:15:06 +0100

    Adjust the progress bar calculations

--- v2/progress.go
+++ v2/progress.go
@@ -9,13 +9,13 @@ func (e *Editor) DrawProgress(c *vt100.Canvas) {
 	var (
 		canvasWidth   = c.Width()
 		canvasHeight  = float64(c.Height())
-		lineNumberTop = float64(e.LineNumber())
+		lineNumberTop = float64(e.LineIndex())
 		allLines      = float64(e.Len())
 		x             = canvasWidth - 1
 		y             = canvasHeight - 1
 	)
 	if allLines > 0 {
-		y = (canvasHeight * lineNumberTop * (allLines - canvasHeight)) / (allLines * allLines)
+		y = (canvasHeight * lineNumberTop * (allLines + canvasHeight)) / (allLines * allLines)
 	}
 	if x >= canvasWidth {
 		x = canvasWidth - 1
