e168894d6ba3d59ca588649e0b72f40f5c89b68d
Alexander F. Rødseth <rodseth@gmail.com>
2023-12-18 18:03:30 +0100

    Update a test

--- v2/toimage_test.go
+++ v2/toimage_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/xyproto/vt100"
 )
 
-func TestToImage(t *testing.T) {
+func TestToImage(_ *testing.T) {
+	// Just check that .ToImage() is available and possible to call
 	vt100.NewCanvas().ToImage()
 }
