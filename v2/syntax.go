aec3b912a7c48ce7fbe438966420bca25fd34030
Alexander F. Rødseth <rodseth@gmail.com>
2024-01-12 19:58:45 +0100

    Minor changes to the C syntax highlighting

--- v2/syntax.go
+++ v2/syntax.go
@@ -286,7 +286,7 @@ func adjustSyntaxHighlightingKeywords(m mode.Mode) {
 		fallthrough // Continue to C/C++ and then to the default
 	case mode.Arduino, mode.C, mode.Cpp:
 		addKeywords := []string{"int8_t", "uint8_t", "int16_t", "uint16_t", "int32_t", "uint32_t", "int64_t", "uint64_t", "size_t"}
-		delKeywords := []string{"static"} // static is treated separately, as a special keyword
+		delKeywords := []string{"ret", "static"} // static is treated separately, as a special keyword
 		addAndRemoveKeywords(addKeywords, delKeywords)
 		fallthrough // Continue to the default
 	default:
