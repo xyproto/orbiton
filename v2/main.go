7b304230958e54068a4ce600cc1d1b7333f6739c
Alexander F. Rødseth <rodseth@gmail.com>
2024-09-04 15:35:23 +0200

    New minor release

--- v2/main.go
+++ v2/main.go
@@ -19,7 +19,7 @@ import (
 	"github.com/xyproto/vt100"
 )
 
-const versionString = "Orbiton 2.66.0"
+const versionString = "Orbiton 2.67.0"
 
 var (
 	// quitMut disallows Exit(1) while a file is being saved
--- web/docker.sh
+++ web/docker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DOCKER_TAG='2.66.0'
+DOCKER_TAG='2.67.0'
 
 # Google Cloud
 PROJECT_ID='personal-306017'
