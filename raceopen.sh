171160b8b7eeda6815f285a4c26be559175e032f
Alexander F. Rødseth <rodseth@gmail.com>
2024-10-15 09:53:14 +0200

    Adjust a test script so that it also works on macOS

--- raceopen.sh
+++ raceopen.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 filename=${1:-v2/main.go}
-find -name "o.log*" -exec rm -v -- {} \;
+find -name "o.log*" -exec rm -v -- {} \; 2>/dev/null || rm -f -v o.log*
 export GORACE="halt_on_error=1 log_path=$PWD/o.log"
 cd v2
 go build -v -race -o ../o
 cd ..
 ./o -f -- "$filename"
 reset
-find -name "o.log*" -exec bat -- {} \;
+find -name "o.log*" -exec bat -- {} \; 2>/dev/null || bat o.log*
