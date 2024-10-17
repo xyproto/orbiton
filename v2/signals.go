ab15ee1f02829015705f3e255df996704ecb9419
Alexander F. Rødseth <rodseth@gmail.com>
2024-10-17 11:24:57 +0200

    Fix the signal handling

--- v2/signals.go
+++ v2/signals.go
@@ -11,43 +11,37 @@ import (
 	"github.com/xyproto/vt100"
 )
 
-// SetUpSignalHandlers sets up a signal handler for when ctrl-c is pressed (SIGTERM),
-// and also for when SIGUSR1 or SIGWINCH is received.
+var cancelPreviousSignalHandler context.CancelFunc
+
+// SetUpSignalHandlers sets up signal handlers for SIGTERM, SIGUSR1, and SIGWINCH.
 func (e *Editor) SetUpSignalHandlers(c *vt100.Canvas, tty *vt100.TTY, status *StatusBar) {
 
-	// For the drawing and the statusbar
+	// Cancel the previous signal handler if it exists
+	if cancelPreviousSignalHandler != nil {
+		cancelPreviousSignalHandler()
+	}
+
 	resizeMut.Lock()
 	mut.Lock()
 
 	sigChan := make(chan os.Signal, 1)
-	done := make(chan struct{})
+	ctx, cancel := context.WithCancel(context.Background())
+	cancelPreviousSignalHandler = cancel
 
-	// Send a SIGWINCH signal to the parent process if "OG" is set,
-	// to signal that "o is ready" to resize. The "og" GUI will then
-	// send SIGWINCH back, which will trigger FullResetRedraw in the case below.
+	// Handle signals differently for VTEGUI
 	if inVTEGUI {
-		// Clear any previous terminate or USR handlers
 		signal.Reset(syscall.SIGTERM, syscall.SIGUSR1, syscall.SIGWINCH)
-		// Set up notifications
 		signal.Notify(sigChan, syscall.SIGTERM, syscall.SIGUSR1, syscall.SIGWINCH)
-		// Send a SIGWINCH signal to the "og" GUI, which is catched there
 		syscall.Kill(os.Getppid(), syscall.SIGWINCH)
 	} else {
-		// Start these in the background, since the "og" GUI isn't waiting
 		defer func() {
-			// Clear any previous terminate or USR handlers
 			signal.Reset(syscall.SIGTERM, syscall.SIGUSR1, syscall.SIGWINCH)
-			// Set up notifications
 			signal.Notify(sigChan, syscall.SIGTERM, syscall.SIGUSR1, syscall.SIGWINCH)
 		}()
 	}
 
-	// For the drawing and the statusbar
 	resizeMut.Unlock()
 	mut.Unlock()
-
-	// Using a context to make it easy stop the goroutine loop below
-	ctx, cancel := context.WithCancel(context.Background())
 
 	go func() {
 		defer cancel()
@@ -56,36 +50,24 @@ func (e *Editor) SetUpSignalHandlers(c *vt100.Canvas, tty *vt100.TTY, status *St
 			case sig := <-sigChan:
 				switch sig {
 				case syscall.SIGTERM:
-					// Save the file
 					mut.Lock()
 					e.UserSave(c, tty, status)
 					mut.Unlock()
 				case syscall.SIGUSR1:
-					// Unlock the file
 					if absFilename, err := filepath.Abs(e.filename); err != nil {
-						// Just unlock the non-absolute filename
 						fileLock.Unlock(e.filename)
 					} else {
 						fileLock.Unlock(absFilename)
 					}
 					fileLock.Save()
 				case syscall.SIGWINCH:
-					// Full redraw, like if Esc was pressed
-					drawLines := true
-					e.FullResetRedraw(c, status, drawLines, false)
-					// Try twice
+					e.FullResetRedraw(c, status, true, false)
 					time.Sleep(300 * time.Millisecond)
-					e.FullResetRedraw(c, status, drawLines, false)
+					e.FullResetRedraw(c, status, true, false)
 				}
 			case <-ctx.Done():
 				return
 			}
 		}
-	}()
-
-	defer func() {
-		close(done)
-		signal.Reset()
-		cancel()
 	}()
 }
