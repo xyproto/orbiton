19d759cee0ab0fb04195df4b601d134e966eaf21
Alexander F. Rødseth <rodseth@gmail.com>
2024-10-20 12:49:15 +0200

    Add a function for previewing man pages

--- v2/quit.go
+++ v2/quit.go
@@ -8,6 +8,7 @@ import (
 	"syscall"
 
 	"github.com/xyproto/env/v2"
+	"github.com/xyproto/files"
 	"github.com/xyproto/textoutput"
 	"github.com/xyproto/vt100"
 )
@@ -74,4 +75,23 @@ func quitExecShellCommand(tty *vt100.TTY, workDir string, shellCommand string) {
 	defer quitMut.Unlock()
 	_ = os.Chdir(workDir)
 	syscall.Exec(shellExecutable, []string{shellExecutable, "-c", shellCommand}, env.Environ())
+}
+
+func execMan(tty *vt100.TTY, workDir, manPageFilename string) {
+	if tty != nil {
+		tty.Close()
+	}
+	vt100.Reset()
+	vt100.Clear()
+	vt100.Close()
+	vt100.ShowCursor(true)
+	vt100.SetXY(uint(0), uint(1))
+	const shellExecutable = "/bin/sh"
+	quitMut.Lock()
+	defer quitMut.Unlock()
+	_ = os.Chdir(workDir)
+	env.Set("MANPAGER", os.Args[0])
+	manExecutable := files.Which("man")
+	// TODO: Do not use syscall.Exec here, but rather launch a child process
+	syscall.Exec(manExecutable, []string{manExecutable, "-l", manPageFilename}, env.Environ())
 }
