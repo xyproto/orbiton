2404ff4ea18bd130961bb0d1e8776e27cc440e29
Alexander F. Rødseth <rodseth@gmail.com>
2024-10-30 00:16:04 +0100

    Simplify the man page syntax highlighting a bit

--- v2/man.go
+++ v2/man.go
@@ -53,7 +53,6 @@ func (e *Editor) manPageHighlight(line string, firstLine, lastLine bool) string 
 		hasAnyWords     = hasWords(trimmedLine)
 		innerSpaceCount = strings.Count(trimmedLine, " ")
 	)
-
 	if strings.Count(trimmedLine, "  ") > 10 && (firstLine || lastLine) { // first and last line
 		return e.CommentColor.Get(line)
 	}
@@ -72,7 +71,6 @@ func (e *Editor) manPageHighlight(line string, firstLine, lastLine bool) string 
 				// One or two command line options, color them differently
 				return e.MenuArrowColor.Get(line)
 			}
-
 			if !foundLetter && (unicode.IsLetter(r) || r == '_') {
 				foundLetter = true
 			}
@@ -84,26 +82,20 @@ func (e *Editor) manPageHighlight(line string, firstLine, lastLine bool) string 
 			} else {
 				spaceCount = 0
 			}
-
 			if r != ' ' && (prevR == ' ' || prevR == '-') && (r == '-' || r == '[' || r == '_') && (prevR == '-' || !inFlag) {
 				inFlag = true
 				rs = append(rs, []rune(off+e.MenuArrowColor.String())...)
-				rs = append(rs, r)
 			} else if (prevR == ' ' || prevR == '-') && (r == '-' || r == '[' || r == ']' || r == '_') && inFlag {
-				rs = append(rs, r)
+				// do nothing
 			} else if inFlag { // Color the rest of the flag text in the textColor color (LightBlue)
 				inFlag = false
 				rs = append(rs, []rune(off+e.MarkdownTextColor.String())...)
-				rs = append(rs, r)
 			} else if foundLetter && spaceCount > 2 { // Color the rest of the line in the foreground color (LightGreen)
 				rs = append(rs, []rune(off+normal.String())...)
-				rs = append(rs, r)
 			} else if r == ']' { // Color the rest of the line in the comment color (DarkGray)
 				rs = append(rs, []rune(off+e.CommentColor.String())...)
-				rs = append(rs, r)
-			} else {
-				rs = append(rs, r)
 			}
+			rs = append(rs, r)
 			prevR = r
 		}
 		rs = append(rs, []rune(off)...)
@@ -121,19 +113,15 @@ func (e *Editor) manPageHighlight(line string, firstLine, lastLine bool) string 
 		inAngles bool
 		nextRune rune
 	)
-
 	rs = append(rs, []rune(normal.String())...)
 	hasAlpha := strings.Contains(trimmedLine, "@")
 	lineRunes := []rune(line)
-
 	for i, r := range line {
-
 		if (i + 1) < len(lineRunes) {
 			nextRune = lineRunes[i+1]
 		} else {
 			nextRune = ' '
 		}
-
 		if (unicode.IsLetter(r) || r == '_') && !inWord {
 			inWord = true
 		} else if inWord && !unicode.IsLetter(r) && !hexDigit(r) {
@@ -147,31 +135,21 @@ func (e *Editor) manPageHighlight(line string, firstLine, lastLine bool) string 
 		if !inWord && unicode.IsDigit(r) && !inDigits {
 			inDigits = true
 			rs = append(rs, []rune(off+e.ItalicsColor.String())...)
-			rs = append(rs, r)
 		} else if inDigits && hexDigit(r) {
-			rs = append(rs, r)
+			// do nothing
 		} else if !inWord && inDigits {
 			inDigits = false
 			rs = append(rs, []rune(off+normal.String())...)
-			rs = append(rs, r)
 		} else if !inWord && (r == '*' || r == '$' || r == '%' || r == '!' || r == '/' || r == '=' || r == '-') {
 			rs = append(rs, []rune(off+e.MenuArrowColor.String())...)
-			rs = append(rs, r)
 		} else if r == '@' { // color @ gray and the rest of the string white
 			rs = append(rs, []rune(off+e.CommentColor.String())...)
-			rs = append(rs, r)
-			rs = append(rs, []rune(off+e.ItalicsColor.String())...)
 		} else if hasAlpha && r == '<' { // color < gray and the rest of the string white
 			rs = append(rs, []rune(off+e.CommentColor.String())...)
-			rs = append(rs, r)
-			rs = append(rs, []rune(off+e.ItalicsColor.String())...)
 		} else if hasAlpha && r == '>' { // color > gray and the rest of the string normal
 			rs = append(rs, []rune(off+e.CommentColor.String())...)
-			rs = append(rs, r)
-			rs = append(rs, []rune(off+normal.String())...)
 		} else if inAngles || r == '>' {
 			rs = append(rs, []rune(off+e.ItalicsColor.String())...)
-			rs = append(rs, r)
 		} else if inWord && unicode.IsUpper(prevRune) && ((unicode.IsUpper(r) && unicode.IsLetter(nextRune)) || (unicode.IsLower(r) && unicode.IsUpper(prevRune) && !unicode.IsLetter(nextRune))) {
 			if unicode.IsUpper(r) {
 				// This is for the leading and trailing letter of uppercase words
@@ -179,7 +157,6 @@ func (e *Editor) manPageHighlight(line string, firstLine, lastLine bool) string 
 			} else {
 				rs = append(rs, []rune(off+e.MarkdownTextColor.String())...)
 			}
-			rs = append(rs, r)
 		} else if inWord && (unicode.IsUpper(r) || (unicode.IsUpper(prevRune) && unicode.IsLetter(r))) {
 			if !unicode.IsLower(r) && (((unicode.IsUpper(nextRune) || nextRune == ' ') && unicode.IsLetter(prevRune)) || unicode.IsUpper(nextRune) || !unicode.IsLetter(nextRune)) {
 				// This is for the center letters of uppercase words
@@ -187,15 +164,16 @@ func (e *Editor) manPageHighlight(line string, firstLine, lastLine bool) string 
 			} else {
 				rs = append(rs, []rune(off+e.MarkdownTextColor.String())...)
 			}
-			rs = append(rs, r)
 		} else if inWord && unicode.IsUpper(r) {
 			rs = append(rs, []rune(off+e.ImageColor.String())...)
-			rs = append(rs, r)
 		} else if !inWord || !unicode.IsUpper(r) {
 			rs = append(rs, []rune(off+e.MarkdownTextColor.String())...)
-			rs = append(rs, r)
-		} else {
-			rs = append(rs, r)
+		}
+		rs = append(rs, r)
+		if r == '@' || (hasAlpha && r == '<') { // color @ or < gray and the rest of the string white
+			rs = append(rs, []rune(off+e.ItalicsColor.String())...)
+		} else if hasAlpha && r == '>' { // color > gray and the rest of the string normal
+			rs = append(rs, []rune(off+normal.String())...)
 		}
 		prevRune = r
 	}
