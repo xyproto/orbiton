ca3b708ac8c62f755266f099400194e7de2e1ccf
Alexander F. Rødseth <rodseth@gmail.com>
2024-11-20 11:48:09 +0100

    Add a new utility function

---
+++ v2/bytes.go
@@ -0,0 +1,31 @@
+package main
+
+import "bytes"
+
+func containsInTheFirstNLines(data []byte, n int, target []byte) bool {
+	var (
+		lineCount int
+		lineStart int
+		line      []byte
+	)
+	for i := 0; i < len(data); i++ {
+		if data[i] == '\n' {
+			line = data[lineStart:i]
+			if bytes.Contains(line, target) {
+				return true
+			}
+			lineCount++
+			if lineCount >= n {
+				return false
+			}
+			lineStart = i + 1
+		}
+	}
+	if lineCount < n && lineStart < len(data) {
+		line = data[lineStart:]
+		if bytes.Contains(line, target) {
+			return true
+		}
+	}
+	return false
+}
