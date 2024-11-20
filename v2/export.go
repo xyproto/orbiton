6191142ff2601f771d3a31710b966ea9e83fbf4b
Alexander F. Rødseth <rodseth@gmail.com>
2024-11-20 11:52:05 +0100

    Look up the path to asciidoctor and scdoc and cache it

--- v2/export.go
+++ v2/export.go
@@ -13,9 +13,14 @@ import (
 
 // exportScdoc tries to export the current document as a manual page, using scdoc
 func (e *Editor) exportScdoc(manFilename string) error {
-	scdoc := exec.Command("scdoc")
+	scdocPath := files.WhichCached("scdoc")
+	if scdocPath == "" {
+		return errors.New("could not find scdoc in the PATH")
+	}
 
-	// Place the current contents in a buffer, and feed it to stdin to the command
+	scdoc := exec.Command(scdocPath)
+
+	// Place the current contents in a buffer, and feed it by stdin to the command
 	var buf bytes.Buffer
 	buf.WriteString(e.String())
 	scdoc.Stdin = &buf
@@ -34,39 +39,36 @@ func (e *Editor) exportScdoc(manFilename string) error {
 	saveCommand(scdoc)
 
 	// Run scdoc
-	if err := scdoc.Run(); err != nil {
+	err = scdoc.Run()
+	if err != nil {
 		errorMessage := strings.TrimSpace(errBuf.String())
 		if len(errorMessage) > 0 {
-			return errors.New(errorMessage)
+			err = errors.New(errorMessage)
 		}
-		return err
 	}
-	return nil
+	return err
 }
 
 // exportAdoc tries to export the current document as a manual page, using asciidoctor
 func (e *Editor) exportAdoc(c *vt100.Canvas, tty *vt100.TTY, manFilename string) error {
+	adocPath := files.WhichCached("asciidoctor")
+	if adocPath == "" {
+		return errors.New("could not find asciidoctor in the PATH")
+	}
+
 	// TODO: Use a proper function for generating temporary files
 	tmpfn := "___o___.adoc"
 	if files.Exists(tmpfn) {
 		return errors.New(tmpfn + " already exists, please remove it")
 	}
 
-	// TODO: Write a SaveAs function for the Editor
-	oldFilename := e.filename
-	e.filename = tmpfn
-	err := e.Save(c, tty)
-	if err != nil {
-		e.filename = oldFilename
-		return err
-	}
-	e.filename = oldFilename
+	e.SaveAs(c, tty, tmpfn)
 
 	// Run asciidoctor
-	adocCommand := exec.Command("asciidoctor", "-b", "manpage", "-o", manFilename, tmpfn)
+	adocCommand := exec.Command(adocPath, "-b", "manpage", "-o", manFilename, tmpfn)
 	saveCommand(adocCommand)
-	if err = adocCommand.Run(); err != nil {
-		_ = os.Remove(tmpfn) // Try removing the temporary filename if pandoc fails
+	if err := adocCommand.Run(); err != nil {
+		_ = os.Remove(tmpfn) // Try removing the temporary filename if asciidoctor fails
 		return err
 	}
 	return os.Remove(tmpfn)
