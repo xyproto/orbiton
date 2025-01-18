e4a500f5d442e8098bf061d39c0ee4df8aab1a37
Alexander F. Rødseth <rodseth@gmail.com>
2025-01-18 16:32:08 +0100

    New patch release

--- v2/main.go
+++ v2/main.go
@@ -22,7 +22,7 @@ import (
 )
 
 const (
-	versionString = "Orbiton 2.68.6"
+	versionString = "Orbiton 2.68.7"
 
 	// How many lines of context above and below should the tab completion try to use?
 	ollamaContextLines = 20
--- web/docker.sh
+++ web/docker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DOCKER_TAG='2.68.6'
+DOCKER_TAG='2.68.7'
 
 # Google Cloud
 PROJECT_ID='personal-306017'
