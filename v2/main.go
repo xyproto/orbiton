72860a717a6949a525c35a82ba802fde98f1952e
Alexander F. Rødseth <rodseth@gmail.com>
2025-06-09 14:12:54 +0200

    New release

--- v2/main.go
+++ v2/main.go
@@ -22,7 +22,7 @@ import (
 )
 
 const (
-	versionString = "Orbiton 2.69.0"
+	versionString = "Orbiton 2.70.0"
 )
 
 var (
--- web/docker.sh
+++ web/docker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DOCKER_TAG='2.69.0'
+DOCKER_TAG='2.70.0'
 
 # Google Cloud
 PROJECT_ID='personal-306017'
