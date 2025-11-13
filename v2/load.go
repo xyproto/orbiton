1ce7f915077411bc64f5be1978dc713e536aa243
Alexander F. Rødseth <rodseth@gmail.com>
2025-11-13 12:19:38 +0100

    Disable the JPEGXL support for now, beacuse of size + warnings
    The executable size grew too much, and there were warnings about purego

--- v2/load.go
+++ v2/load.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 
 	ico "github.com/dkua/go-ico"
-	"github.com/gen2brain/jpegxl"
+	//"github.com/gen2brain/jpegxl"
 	bmp "github.com/jsummers/gobmp"
 	"github.com/xfmoulet/qoi"
 	"golang.org/x/image/webp"
@@ -36,8 +36,8 @@ func LoadImage(filename string) (*image.NRGBA, error) {
 		img, err = ico.Decode(f)
 	case ".jpg", ".jpeg":
 		img, err = jpeg.Decode(f)
-	case ".jxl":
-		img, err = jpegxl.Decode(f)
+	//case ".jxl":
+	//img, err = jpegxl.Decode(f)
 	case ".png":
 		img, err = png.Decode(f)
 	case ".qoi":
