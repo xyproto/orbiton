b72067020fa25c2c6be38ef7dc5bf0e3b3bf81d3
Alexander F. Rødseth <rodseth@gmail.com>
2026-03-03 11:10:32 +0100

    Adjust the recording script and tape files

--- rec/rec.sh
+++ rec/rec.sh
@@ -5,11 +5,15 @@ cd "$(dirname "$0")"
 
 export VHS_NO_SANDBOX=1
 
-for lang in c cpp rust zig python; do
+for lang in c cpp rust zig python bash; do
   rm -f /tmp/main.* /tmp/tmp
   vhs simple_$lang.tape
 done
 
+# Final cleanup
 rm -f /tmp/main.* /tmp/tmp
+
+# Move all gifs to the gif directory
+mv -f -v *.gif gif/
 
 echo 'All recordings done.'
