dc23b98c54ba8722e27e9a0e85b4fb726ab028e5
Alexander F. Rødseth <rodseth@gmail.com>
2026-03-03 14:28:26 +0100

    Update the rec script

--- rec/rec.sh
+++ rec/rec.sh
@@ -5,15 +5,13 @@ cd "$(dirname "$0")"
 
 export VHS_NO_SANDBOX=1
 
-for lang in c cpp rust zig python bash; do
-  rm -f /tmp/main.* /tmp/tmp
+rm -rf ./tmp
+mkdir -p tmp
+for lang in c cpp rust zig python bash odin go; do
   vhs simple_$lang.tape
+  mv -v simple_$lang.gif gif/
+  rm -rf ./tmp
+  mkdir -p tmp
 done
-
-# Final cleanup
-rm -f /tmp/main.* /tmp/tmp
-
-# Move all gifs to the gif directory
-mv -f -v *.gif gif/
 
 echo 'All recordings done.'
