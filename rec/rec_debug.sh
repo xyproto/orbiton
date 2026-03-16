547e4728e3e8876971363ba3892c3d66f9332e3e
Alexander F. Rødseth <rodseth@gmail.com>
2026-03-16 16:06:24 +0100

    Update vhs scripts

--- rec/rec_debug.sh
+++ rec/rec_debug.sh
@@ -1,17 +1,16 @@
-#!/bin/bash
-set -e
+#!/usr/bin/env bash
 
 cd "$(dirname "$0")"
 
 export VHS_NO_SANDBOX=1
 
-VHS=$(which vhs)
+VHS=$(which vhs 2>/dev/null || true)
 if [ -x "$HOME/clones/vhs/build/vhs" ]; then
   VHS="$HOME/clones/vhs/build/vhs"
 elif [ -x "$HOME/clones/vhs/vhs" ]; then
   VHS="$HOME/clones/vhs/vhs"
 fi
-if [ ! -x $VHS ]; then
+if [ ! -x "$VHS" ]; then
   echo 'Could not find vhs'
   exit 1
 fi
