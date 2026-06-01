93bfa41a228f20553f101d7fc83626640e3f4cb8
Alexander F. Rødseth <rodseth@gmail.com>
2026-06-01 10:34:15 +0200

    Refactor

---
+++ v2/ctypes.go
@@ -0,0 +1,9 @@
+package main
+
+var (
+	cTypes          = []string{"bool", "char", "const", "constexpr", "double", "float", "inline", "int", "int16_t", "int32_t", "int64_t", "int8_t", "long", "short", "signed", "size_t", "static", "uint", "uint16_t", "uint32_t", "uint64_t", "uint8_t", "unsigned", "void", "volatile"}
+	cExtensions     = []string{".c", ".cc", ".cpp", ".cxx", ".h", ".hpp", ".m", ".mm"}
+	cControlFlow    = []string{"break", "case", "catch", "continue", "default", "do", "else", "for", "goto", "if", "return", "switch", "while"}
+	cModifiers      = []string{"explicit", "extern", "noexcept", "override", "virtual"}
+	cCompositeTypes = []string{"enum", "struct", "union"}
+)
